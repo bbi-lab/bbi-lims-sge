737e6ab66bd89285293199ba133a8d2fb4703a12
David Reinhart <davidrr@uw.edu>
2024-10-29 15:00:14 -0700

    Generate resusable JSON schema property for user groups, add logic to update JSON schema with many relations

--- server/utils/jsonSchema.ts
+++ server/utils/jsonSchema.ts
@@ -1,18 +1,28 @@
-import { JsonSchema7Type } from "zod-to-json-schema"
-import {getAllVerifiedUsersInfo} from '@/server/services/user-services'
+import zodToJsonSchema, { JsonSchema7AnyType, JsonSchema7ArrayType, JsonSchema7SetType, JsonSchema7Type } from "zod-to-json-schema"
+import {getAllVerifiedUsersInfo, getUserGroups} from '@/server/services/user-services'
 import { users } from '@/server/db/schema/user'
 import _ from 'lodash'
-import { RelationsConfig } from "./db"
+import { RelationsConfig, getRecordsFromTable} from "./db"
+import { PgTableWithColumns } from "drizzle-orm/pg-core"
+import { createSelectSchema } from "drizzle-zod"
 
 export async function refineJsonSchema(jsonSchema:JsonSchema7Type, relationsConfig: RelationsConfig) {
 
     // define JSON schema property as coded list of users, to be applied to JSON schema
     const usersInfo = await getAllVerifiedUsersInfo()
-    const usersJsonSchemaProperty = {
+    const usersJsonSchemaProperty:JsonSchema7AnyType = {
       type: 'string',
       oneOf: [{ const: null, title: '(none)' }, ..._.map(usersInfo, (x) => { return { const: x.id, title: x.name } })],
     }
 
+    // define JSON schema property to select a user group
+    const userGroups = await getUserGroups()
+    const userGroupsJsonSchemaProperty:JsonSchema7AnyType = {
+      type: 'string',
+      oneOf: _.map(userGroups, (x) => { return { const: x.id, title: x.name } }),
+    }
+
+    // iterate over properties and replace one-to-many relations with corresponding JsonSchema property
     const properties = _.get(jsonSchema, 'properties', [])
     for (const property in properties) {
       if (relationsConfig.one && Object.keys(relationsConfig.one).includes(property)) {
@@ -20,5 +30,18 @@ export async function refineJsonSchema(jsonSchema:JsonSchema7Type, relationsConf
           _.set(jsonSchema, ['properties', property], usersJsonSchemaProperty)
         }
       }
+    }
+
+    // iterate over many-to-many relations and add each to JSON schema as a new array property
+    for (const [key, val] of Object.entries(relationsConfig.many)) {
+      const items = await getRecordsFromTable(val.table)
+      const itemsZodSchema = createSelectSchema(val.table)
+      const itemsJsonSchema = zodToJsonSchema(itemsZodSchema)
+
+      const jsonSchemaArrayProperty:JsonSchema7ArrayType = {
+         type: 'array',
+         items: itemsJsonSchema
+      }
+      _.set(jsonSchema, ['properties', key], jsonSchemaArrayProperty)
     }
 }
