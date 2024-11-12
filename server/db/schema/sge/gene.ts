d313d8e15eaa66c91f55bcce659be150c96c88c2
David Reinhart <davidrr@uw.edu>
2024-11-12 11:30:06 -0800

    Add genes table to db matching stucture of NCBI data source
    Adding genes table to be populated with 68325 rows from NCBI dataset: Genes annotated on Homo sapiens (human) GRCh38.p14 (GCF_000001405.40)
    
    Including an UUID field as primary key, and assembly and annotation columns to identify the source (in anticipation of importing other assemblies/annotations later).

--- server/db/schema/sge/gene.ts
+++ server/db/schema/sge/gene.ts
@@ -1,9 +1,23 @@
-import { pgTable, PgTableWithColumns, uuid, varchar, integer } from 'drizzle-orm/pg-core'
+import { pgTable, PgTableWithColumns, pgEnum, uuid, varchar, integer } from 'drizzle-orm/pg-core'
 import _ from 'lodash'
 
+export const geneOrientationEnum = pgEnum('gene_orientations', ['plus', 'minus'])
+
 export const genes: PgTableWithColumns<any> = pgTable('genes', {
-  id: uuid('id').notNull().primaryKey().defaultRandom(),
-  hgnc_id: integer('hgnc_id'),
-  symbol: varchar('symbol', { length: 20 }),
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
+  ncbiAccession: varchar('ncbi_accession', { length: 50 }),
+  startPosition: integer('start_position'),
+  endPosition: integer('end_position'),
+  chromosome: varchar('chromosome', { length: 10 }),
+  orientation: geneOrientationEnum('orientation'),
   name: varchar('name', { length: 255 }),
+  symbol: varchar('symbol', { length: 50 }),
+  geneId: integer('gene_id'),
+  geneType: varchar('gene_type', { length: 50 }),
+  transcriptsAccession: varchar('transcripts_accession', { length: 50 }),
+  proteinAcccession: varchar('protein_acccession', { length: 50 }),
+  proteinLength: integer('protein_length'),
+  locusTag: varchar('locus_tag', { length: 50 }),
+  assembly: varchar('assembly', {length: 50}),
+  annotation: varchar('annotation', {length: 50}),
 })
