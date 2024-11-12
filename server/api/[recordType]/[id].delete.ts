d67e6319421b94ee9b0001822f1a98dc5bc6671c
David Reinhart <davidrr@uw.edu>
2024-11-12 14:55:28 -0800

    Add DELETE endpoints to API for generic record types

---
+++ server/api/[recordType]/[id].delete.ts
@@ -0,0 +1,24 @@
+import { deleteRecord } from '~/server/services/generic-services'
+import _ from 'lodash'
+
+export default defineEventHandler(async (event) => {
+    const { recordType, id } = event.context.params as {recordType: string, id: string}
+
+    try {
+         // requires relevant schema to have been passed on drizzle db init
+        const table = _.get(db, ['query', recordType, 'table'])
+        
+        if (!table) throw createError({
+            statusCode: 500, 
+            statusMessage: `Could not find table, check to make sure ${recordType} is included in drizzle db schemas`
+        })
+        const deletedRecord = await deleteRecord(table, id)
+
+        return deletedRecord
+    } catch (e: any) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: e.message
+        })
+    }
+})
