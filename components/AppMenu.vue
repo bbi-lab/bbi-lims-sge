280dde0ec01a40963d31b798ef2715549bbbb2ba
David Reinhart <davidrr@uw.edu>
2024-11-14 14:18:30 -0800

    Add regions page to menu and collapse UI samples

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -6,8 +6,8 @@ const model = ref([
             { label: 'Projects', icon: 'pi pi-fw pi-home', to: '/sge/projects' },
             { label: 'Targets', icon: 'pi pi-fw pi-bullseye', to: '/sge/targets' },
             { label: 'Genes', icon: 'pi pi-fw pi-list', to: '/sge/genes' },
+            { label: 'Regions', icon: 'pi pi-fw pi-map', to: '/sge/regions' },
             { label: 'PCR experiments', icon: 'pi pi-fw pi-qrcode', to: '/sge/pcr-experiments' },
-            
         ]
     },
     {
@@ -18,137 +18,139 @@ const model = ref([
         ]
     },
     {
-        label: 'UI Components',
-        items: [
-            { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
-            { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
-            { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
-            { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
-            { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
-            { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
-            { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
-            { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
-            { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
-            { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu' },
-            { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
-            { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
-            { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
-            { label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/uikit/timeline' },
-            { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
-        ]
-    },
-    {
-        label: 'Sample Pages',
-        icon: 'pi pi-fw pi-briefcase',
-        to: '/',
-        items: [
-            {
-                label: 'Dashboard',
-                icon: 'pi pi-fw pi-home',
-                to: '/landing'
-            },
-            {
-                label: 'Landing',
-                icon: 'pi pi-fw pi-globe',
-                to: '/landing'
-            },
-            {
-                label: 'Auth',
-                icon: 'pi pi-fw pi-user',
-                items: [
-                    {
-                        label: 'Login',
-                        icon: 'pi pi-fw pi-sign-in',
-                        to: '/uikit/auth/login'
-                    },
-                    {
-                        label: 'Error',
-                        icon: 'pi pi-fw pi-times-circle',
-                        to: '/uikit/auth/error'
-                    },
-                    {
-                        label: 'Access Denied',
-                        icon: 'pi pi-fw pi-lock',
-                        to: '/uikit/auth/access'
-                    }
-                ]
-            },
-            {
-                label: 'Crud',
-                icon: 'pi pi-fw pi-pencil',
-                to: '/crud'
-            },
-            {
-                label: 'Not Found',
-                icon: 'pi pi-fw pi-exclamation-circle',
-                to: '/notfound'
-            },
-            {
-                label: 'Empty',
-                icon: 'pi pi-fw pi-circle-off',
-                to: '/empty'
-            }
-        ]
-    },
-    {
-        label: 'Hierarchy',
-        items: [
-            {
-                label: 'Submenu 1',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Submenu 1.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 1.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                    }
-                ]
-            },
-            {
-                label: 'Submenu 2',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Submenu 2.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        label: 'Get Started',
-        items: [
-            {
-                label: 'Documentation',
-                icon: 'pi pi-fw pi-book',
-                to: '/documentation'
-            },
-            {
-                label: 'View Source',
-                icon: 'pi pi-fw pi-github',
-                url: 'https://github.com/primefaces/sakai-vue',
-                target: '_blank'
-            }
-        ]
+        label: 'UI Showcase',
+        items: [{
+            label: 'UI Components',
+            items: [
+                { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
+                { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
+                { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
+                { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
+                { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
+                { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
+                { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
+                { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
+                { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
+                { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu' },
+                { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
+                { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
+                { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
+                { label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/uikit/timeline' },
+                { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
+            ]
+        },
+        {
+            label: 'Sample Pages',
+            icon: 'pi pi-fw pi-briefcase',
+            items: [
+                {
+                    label: 'Dashboard',
+                    icon: 'pi pi-fw pi-home',
+                    to: '/landing'
+                },
+                {
+                    label: 'Landing',
+                    icon: 'pi pi-fw pi-globe',
+                    to: '/landing'
+                },
+                {
+                    label: 'Auth',
+                    icon: 'pi pi-fw pi-user',
+                    items: [
+                        {
+                            label: 'Login',
+                            icon: 'pi pi-fw pi-sign-in',
+                            to: '/uikit/auth/login'
+                        },
+                        {
+                            label: 'Error',
+                            icon: 'pi pi-fw pi-times-circle',
+                            to: '/uikit/auth/error'
+                        },
+                        {
+                            label: 'Access Denied',
+                            icon: 'pi pi-fw pi-lock',
+                            to: '/uikit/auth/access'
+                        }
+                    ]
+                },
+                {
+                    label: 'Crud',
+                    icon: 'pi pi-fw pi-pencil',
+                    to: '/crud'
+                },
+                {
+                    label: 'Not Found',
+                    icon: 'pi pi-fw pi-exclamation-circle',
+                    to: '/notfound'
+                },
+                {
+                    label: 'Empty',
+                    icon: 'pi pi-fw pi-circle-off',
+                    to: '/empty'
+                }
+            ]
+        },
+        {
+            label: 'Hierarchy',
+            items: [
+                {
+                    label: 'Submenu 1',
+                    icon: 'pi pi-fw pi-bookmark',
+                    items: [
+                        {
+                            label: 'Submenu 1.1',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [
+                                { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+                                { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+                                { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
+                            ]
+                        },
+                        {
+                            label: 'Submenu 1.2',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                        }
+                    ]
+                },
+                {
+                    label: 'Submenu 2',
+                    icon: 'pi pi-fw pi-bookmark',
+                    items: [
+                        {
+                            label: 'Submenu 2.1',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [
+                                { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+                                { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
+                            ]
+                        },
+                        {
+                            label: 'Submenu 2.2',
+                            icon: 'pi pi-fw pi-bookmark',
+                            items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Get Started',
+            items: [
+                {
+                    label: 'Documentation',
+                    icon: 'pi pi-fw pi-book',
+                    to: '/documentation'
+                },
+                {
+                    label: 'View Source',
+                    icon: 'pi pi-fw pi-github',
+                    url: 'https://github.com/primefaces/sakai-vue',
+                    target: '_blank'
+                }
+            ]
+        }]
     }
 ]);
 </script>
