8e2a8ffc15b16465c1dd7779f782b9f9ccd9e205
David Reinhart <davidrr@uw.edu>
2024-11-15 09:17:41 -0800

    Make target region required field

--- server/db/schema/sge/target.ts
+++ server/db/schema/sge/target.ts
@@ -7,5 +7,5 @@ export const targets: PgTableWithColumns<any> = pgTable('targets', {
   id: uuid('id').notNull().primaryKey().defaultRandom(),
   name: varchar('name', { length: 255 }),
   projectId: uuid('project_id').references(() => projects.id),
-  regionId: uuid('region_id').references(() => regions.id),
+  regionId: uuid('region_id').references(() => regions.id).notNull(),
 })
