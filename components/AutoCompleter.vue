7092653ad22745dcb95a5c8256583f4bbb781fba
David Reinhart <davidrr@uw.edu>
2024-11-14 16:10:50 -0800

    Add property to AutoCompleter component to allow joins in search request

--- components/AutoCompleter.vue
+++ components/AutoCompleter.vue
@@ -6,24 +6,35 @@ const props = defineProps({
   searchBaseUrl: {type: String, required: true},
   searchFields: {type: Array, default: ['name']},
   valueField: {type: String, default: 'id'},
-  displayFields: {type: Array, default: ['name']}
+  displayFields: {type: Array, default: ['name']},
+  searchWithClause: {type: Object},
 })
 
 const modelValue = defineModel()
 const currentValue = ref()
 const suggestions = ref([])
 
+function getDisplayValue(record, delimiter=': ') {
+    const result = []
+    for (const field of props.displayFields) {
+        result.push(_.get(record, field))
+    }
+    return _.join(result, delimiter)
+}
+
 onMounted(async () => {
     if (modelValue.value) {
-        const record = await RecordService.getRecord(props.searchBaseUrl, modelValue.value)
-        currentValue.value = {code: modelValue.value, label: record[props.displayFields[0]] }
+        const record = await RecordService.getRecord(props.searchBaseUrl, modelValue.value, props.searchWithClause)
+        currentValue.value = {code: modelValue.value, label: getDisplayValue(record) }
     }
 })
 
 async function autocompleteSearch(event) {
-    const whereClause = {"startsWith": [{"var": props.searchFields[0]}, event.query] }
-    const filtered = await RecordService.getRecords(props.searchBaseUrl, null, whereClause)
-    suggestions.value = _.map(filtered, (x) => { return {code: x[props.valueField], label: x[props.displayFields[0]] }})
+    const whereClause = props.searchFields.length > 1 ?
+        {"or": _.map(props.searchFields, (x) => { return {"startsWith": [{"var": x}, event.query] } })} :
+        {"startsWith": [{"var": props.searchFields[0]}, event.query] }
+    const filtered = await RecordService.getRecords(props.searchBaseUrl, props.searchWithClause, whereClause)
+    suggestions.value = _.map(filtered, (x) => { return {code: x[props.valueField], label: getDisplayValue(x) }})
 }
 
 function clearValue() {
