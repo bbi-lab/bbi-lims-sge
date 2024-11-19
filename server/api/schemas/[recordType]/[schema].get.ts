3beb7847c1d3a375125cb6ea0a802177e4fddfa5
David Reinhart <davidrr@uw.edu>
2024-11-19 09:03:17 -0800

    REST API: Refine JSON schema based on relations config, if found

--- server/api/schemas/[recordType]/[schema].get.ts
+++ server/api/schemas/[recordType]/[schema].get.ts
@@ -1,15 +1,21 @@
 import { schemas } from '@/server/db/schema/sge/zod'
+import { relationsConfigs } from '@/server/db/schema/sge/relations'
 import _ from 'lodash'
 import { ZodObject, ZodTypeAny } from 'zod'
 import { zodToJsonSchema } from 'zod-to-json-schema'
+import { RelationsConfig } from '~/server/utils/db'
 
 export default defineEventHandler(async (event) => {
     const { recordType, schema } = event.context.params as {recordType: string, schema: string}
     try {
-        const currentSchema = _.get(schemas, [recordType, _.camelCase(schema)]) as ZodObject<any>
+        const currentSchema = _.get(schemas, [_.camelCase(recordType), _.camelCase(schema)]) as ZodObject<any>
         
         // generate JSON Schema from Zod object
         const jsonSchema = zodToJsonSchema(currentSchema, { $refStrategy: 'none' })
+
+        const relationsConfig = _.get(relationsConfigs, _.camelCase(recordType)) as RelationsConfig
+        if (relationsConfig) await refineJsonSchema(jsonSchema, relationsConfig)
+
         return jsonSchema
 
     } catch (e: any) {
