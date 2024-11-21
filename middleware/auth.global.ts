895b706daeec0b9b7deb4146196753ecea5ea841
David Reinhart <davidrr@uw.edu>
2024-11-21 10:58:32 -0800

    Redirect non-admins on admin routes

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,15 +1,20 @@
+import _ from 'lodash'
+
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    const { loggedIn, fetch } = useUserSession()
+    const { loggedIn, user, fetch } = useUserSession()
 
     await fetch()
-    if (!loggedIn.value && !['/register', '/login'].includes(to.path)) {
+    if (!_.get(user.value, 'isAdmin') && to.path.startsWith('/admin/')){
+      return navigateTo('/')
+    } else if (!loggedIn.value && !['/register', '/login'].includes(to.path)) {
       // if not logged in and not currently headed to login page, redirect there
       return navigateTo({path: '/login', query: {from: to.path}})
     } else if (loggedIn.value && ['/register', '/login'].includes(to.path)) {
       // if already logged in and is headed to the login page, redirect to home
       // to prevent infinite redirect
       return navigateTo('/')
-    } 
+    }
+    
     return
   }
 )
