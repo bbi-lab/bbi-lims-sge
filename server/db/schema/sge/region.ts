d8c7dc3190faff5ac52489f091c26be0dfd9a6f2
David Reinhart <davidrr@uw.edu>
2024-11-22 10:33:46 -0800

    Drop region start/end columns, change fixedEdits to varchar array

--- server/db/schema/sge/region.ts
+++ server/db/schema/sge/region.ts
@@ -1,4 +1,5 @@
-import { pgTable, PgTableWithColumns, uuid, varchar, integer, jsonb } from 'drizzle-orm/pg-core'
+import { pgTable, PgTableWithColumns, uuid, varchar, integer } from 'drizzle-orm/pg-core'
+import {sql} from 'drizzle-orm'
 import _ from 'lodash'
 import {genes} from './gene'
 
@@ -6,12 +7,10 @@ export const regions: PgTableWithColumns<any> = pgTable('regions', {
   id: uuid('id').notNull().primaryKey().defaultRandom(),
   name: varchar('name', { length: 255 }).notNull(),
   geneId: uuid('gene_id').references(() => genes.id).notNull(),
-  regionStart: integer('region_start'),
-  regionEnd: integer('region_end'),
   ampliconStart: integer('amplicon_start'),
   ampliconEnd: integer('amplicon_end'),
   ampliconSequence: varchar('amplicon_sequence', {length: 255}),
   snvLibraryStart: integer('snv_library_start'),
   snvLibraryEnd: integer('snv_library_end'),
-  fixedEdits: jsonb('fixed_edits')
+  fixedEdits: varchar('fixedEdits', { length: 255 }).array(),
 })
