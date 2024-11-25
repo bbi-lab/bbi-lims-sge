8d90be8b4c25cb3506f8bb71f26954b1f10c88f2
David Reinhart <davidrr@uw.edu>
2024-11-25 14:03:26 -0800

    Allow default ID value to be passed as query param on schema GET requests

--- server/api/schemas/[recordType]/[schema].get.ts
+++ server/api/schemas/[recordType]/[schema].get.ts
@@ -7,6 +7,8 @@ import { RelationsConfig } from '~/server/utils/db'
 
 export default defineEventHandler(async (event) => {
     const { recordType, schema } = event.context.params as {recordType: string, schema: string}
+    const { id } = getQuery(event) as {id: string}
+    
     try {
         const currentSchema = _.get(schemas, [_.camelCase(recordType), _.camelCase(schema)]) as ZodObject<any>
         
@@ -14,7 +16,7 @@ export default defineEventHandler(async (event) => {
         const jsonSchema = zodToJsonSchema(currentSchema, { $refStrategy: 'none' })
 
         const relationsConfig = _.get(relationsConfigs, _.camelCase(recordType)) as RelationsConfig
-        if (relationsConfig) await refineJsonSchema(jsonSchema, relationsConfig)
+        if (relationsConfig) await refineJsonSchema(jsonSchema, relationsConfig, id)
 
         return jsonSchema
 
