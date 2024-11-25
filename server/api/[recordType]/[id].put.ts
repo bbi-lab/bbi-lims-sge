d46329966a1f27a45bad437484bbe74dfd47ea4c
David Reinhart <davidrr@uw.edu>
2024-11-25 14:08:16 -0800

    Update transfection experiment many-to-many records

--- server/api/[recordType]/[id].put.ts
+++ server/api/[recordType]/[id].put.ts
@@ -2,6 +2,7 @@ import { updateRecord } from '~/server/services/generic-services'
 import _ from 'lodash'
 import { schemas } from '~/server/db/schema/sge/zod'
 import { ZodObject } from 'zod'
+import { updateTargets } from '~/server/services/transfection-experiment-services'
 
 export default defineEventHandler(async (event) => {
     const { recordType, id } = event.context.params as {recordType: string, id: string}
@@ -10,6 +11,12 @@ export default defineEventHandler(async (event) => {
         const body = await readBody(event)
         const updateSchema = schemas[_.camelCase(recordType)].update as ZodObject<any>
         const values = updateSchema.parse(body)
+
+        // many-to-many
+        if (_.camelCase(recordType) == 'transfectionExperiments' && _.isArray(body.transfectionExperimentsTargets)) {
+            await updateTargets(id, _.map(body.transfectionExperimentsTargets, (x) => x.targetId))
+        }
+
         const updatedRecord = await updateRecord(_.get(db, ['query', _.camelCase(recordType), 'table']), id, values)
 
         return updatedRecord
