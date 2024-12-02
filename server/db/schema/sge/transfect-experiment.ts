3bc8f4952f78cf90a79d92e45788ce1f96ec624f
David Reinhart <davidrr@uw.edu>
2024-12-02 09:47:41 -0800

    Change numeric columns to double precision
    The drizzle-zod plugin applys a string type to numeric columns. Switching to double precision so that number type is applied to Zod objects and resulting JSON schemas.

--- server/db/schema/sge/transfect-experiment.ts
+++ server/db/schema/sge/transfect-experiment.ts
@@ -1,4 +1,4 @@
-import { pgTable, PgTableWithColumns, timestamp, uuid, numeric, integer, varchar, text } from 'drizzle-orm/pg-core'
+import { pgTable, PgTableWithColumns, timestamp, uuid, numeric, integer, varchar, text, doublePrecision } from 'drizzle-orm/pg-core'
 import { users } from '../user'
 import { targets } from './target'
 
@@ -13,18 +13,18 @@ export const transfectTargets: PgTableWithColumns<any> = pgTable('transfect_targ
   id: uuid('id').notNull().primaryKey().defaultRandom(),
   experimentId: uuid('experiment_id').references(() => transfectExperiments.id).notNull(),
   targetId: uuid('target_id').references(() => targets.id).notNull(),
-  snvLibraryConc: numeric('snv_library_conc'),
-  snvLibraryTo3ugVol: numeric('snv_library_to_3ug_vol'),
+  snvLibraryConc: doublePrecision('snv_library_conc'),
+  snvLibraryTo3ugVol: doublePrecision('snv_library_to_3ug_vol'),
   sgRna: varchar('sg_rna', { length: 255 }),
-  sgRnaConc: numeric('sg_rna_conc'),
-  sgRnaTo12ugVol: numeric('sg_rna_to_12ug_vol'),
+  sgRnaConc: doublePrecision('sg_rna_conc'),
+  sgRnaTo12ugVol: doublePrecision('sg_rna_to_12ug_vol'),
   sgRnaNegControl: varchar('sg_rna_neg_control', { length: 255 }),
-  hprt1SgRnaConc: numeric('hprt1_sg_rna_conc'),
-  hprt1SgRnaTo12ugVol: numeric('hprt1_sg_rna_to_12ug_vol'),
-  xfectBuffer: numeric('xfect_buffer'),
-  xfectPolymerPerTransfect: numeric('xfect_polymer_per_transfect'),
+  hprt1SgRnaConc: doublePrecision('hprt1_sg_rna_conc'),
+  hprt1SgRnaTo12ugVol: doublePrecision('hprt1_sg_rna_to_12ug_vol'),
+  xfectBuffer: doublePrecision('xfect_buffer'),
+  xfectPolymerPerTransfect: doublePrecision('xfect_polymer_per_transfect'),
   transfectionCount: integer('transfection_count'),
-  snvLibNeeded: numeric('snv_lib_needed'),
-  sgRnaNeeded: numeric('sg_rna_needed'),
+  snvLibNeeded: doublePrecision('snv_lib_needed'),
+  sgRnaNeeded: doublePrecision('sg_rna_needed'),
   notes: text('notes'),
 })
