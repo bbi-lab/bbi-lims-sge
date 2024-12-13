50e5a9533220c7751cd6decc5cc84eff8c52667b
David Reinhart <davidrr@uw.edu>
2024-12-13 10:37:50 -0800

    Apply type to plate fetch response using calculated type

--- pages/sge/plate-diagram/[id].vue
+++ pages/sge/plate-diagram/[id].vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import { RecordService } from '~/utils/service/RecordService'
+
+const route = useRoute()
+
+// alternative methods, both return the same type currently
+// TODO: add support for typing nested properties by specifying columns in "with" object
+// TODO: have type auto-inferred by getRecord function, to avoid repeating ourselves like this 
+type PlateWithWells = InferResultType<'plates', {wells: {columns: {x: true, y: true}}}, {id: true, name: true}>
+// type PlateWithWells = InferQueryModel<'plates', {with: {wells: true}, columns: {id: true, name: true}}>
+
+const plate = await RecordService.getRecordTyped<PlateWithWells>(
+    'plates',
+    route.params.id as string,
+    {wells: {columns: {x: true, y: true}}},
+    {id: true, name: true}
+)
+</script>
 <template>
 
 <PlateDiagram :plateId="$route.params.id"/>
