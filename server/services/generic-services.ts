1161f307ca69385fb8dfaf1dca98e0b245683cf8
David Reinhart <davidrr@uw.edu>
2025-01-30 12:25:14 -0800

    Trim string values submitted through REST API and add stricter typing

--- server/services/generic-services.ts
+++ server/services/generic-services.ts
@@ -7,6 +7,16 @@ import { type PgTableWithColumns } from 'drizzle-orm/pg-core'
 import { eq } from 'drizzle-orm'
 import '../db/schema/sge/relations'
 
+interface RecordValues {[key: string]: string | number | boolean | null | undefined }
+
+function trimObjectValues(records: RecordValues[]): RecordValues[] {
+    return _.map(records, (x) => {
+        return _.mapValues(x, (value) => {
+            return _.isString(value) ? _.trim(value) : value
+        })
+    })
+}
+
 export async function selectRecords(queryBuilder: RelationalQueryBuilder<any, any>, selectParams: SelectParams) {
     const result = await queryBuilder.findMany({
         columns: selectParams.columns,
@@ -24,34 +34,34 @@ export async function selectRecord(queryBuilder: RelationalQueryBuilder<any, any
     return result
 }
 
-export async function insertRecord(table: PgTableWithColumns<any>, values: any) {
+export async function insertRecord(table: PgTableWithColumns<any>, values: RecordValues) {
     const [newRecord] = await db
       .insert(table)
-      .values(values)
+      .values(trimObjectValues([values])[0])
       .returning()
   
     return newRecord
-  }
+}
 
-export async function insertRecords(table: PgTableWithColumns<any>, records: Array<any>) {
+export async function insertRecords(table: PgTableWithColumns<any>, records: Array<RecordValues>) {
     const newRecords = await db
       .insert(table)
-      .values(records)
+      .values(trimObjectValues(records))
       .returning()
   
     return newRecords
   }
-export async function updateRecord(table: PgTableWithColumns<any>, id: any, values: any) {
+export async function updateRecord(table: PgTableWithColumns<any>, id: any, values: RecordValues) {
     const [updatedRecord] = await db
         .update(table)
-        .set(values)
+        .set(trimObjectValues([values])[0])
         .where(eq(table.id, id))
         .returning()
 
     return updatedRecord
 }
 
-export async function deleteRecord(table: PgTableWithColumns<any>, id: any) {
+export async function deleteRecord(table: PgTableWithColumns<any>, id: string | number) {
     const [deletedRecord] = await db
         .delete(table)
         .where(eq(table.id, id))
