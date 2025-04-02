f19342587e0c8f0e236fa89f8f702a6924cf00f3
David Reinhart <davidrr@uw.edu>
2025-04-02 16:00:38 -0700

    f

--- components/QuickTable.client.vue
+++ components/QuickTable.client.vue
@@ -438,8 +438,8 @@ function filterByColumnVisibility(columns: SortedColumnDefinition[]): SortedColu
             <template #body="{ data }">
                 <div class="flex items-start">
                     <template v-for="(v, k) in rowActionsEnd">
-                        <Button v-tooltip.top="v.tooltip" v-if="!v.iconComponent" class="mr-1 mb-1" :icon="v.icon" :iconPos="v.iconPos" :severity="v.severity || 'info'" :label="_.isFunction(v.label) ? v.label(data) : (v.label || _.startCase(_.toString(k)))" @click="v.action(data)" />
-                        <Button v-tooltip.top="v.tooltip" v-if="v.iconComponent" class="mr-1 mb-1" :severity="v.severity || 'info'" :label="v.label ? v.label(data) : _.startCase(k)" @click="v.action(data)">
+                        <Button v-tooltip.top="v.tooltip" v-if="!v.iconComponent" class="mr-1 mb-1" :icon="v.icon" :iconPos="v.iconPos" :severity="v.severity || 'info'" :label="_.isFunction(v.label) ? v.label(data) : (_.has(v, 'label') ? v.label : _.startCase(_.toString(k)))" @click="v.action(data)" />
+                        <Button v-tooltip.top="v.tooltip" v-if="v.iconComponent" class="mr-1 mb-1" :severity="v.severity || 'info'" :label="_.isFunction(v.label) ? v.label(data) : (_.has(v, 'label') ? v.label : _.startCase(_.toString(k)))" @click="v.action(data)">
                             <template #icon>
                                 <span :class="`pi pi-fw p-button-icon ${v.iconPos=='right' ? 'p-button-icon-right' : ''} inline-block`">
                                     <component :is="v.iconComponent" />
