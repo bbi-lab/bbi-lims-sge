8c439abd19693866f30d2099eb5f44e0976f7447
David Reinhart <davidrr@uw.edu>
2025-04-04 12:08:47 -0700

    Fix broken plate diagram test

--- tests/composables/plate-diagram.test.ts
+++ tests/composables/plate-diagram.test.ts
@@ -4,14 +4,22 @@ import _ from 'lodash'
 
 describe('PlateDiagram composable tests', () => {
     it('should create a plate diagram', () => {
-        const plateDiagram = makePlateDiagram({x: 5, y: 5})
-        expect(plateDiagram.wells()).toHaveLength(25)
+        const wells = _.range(0, 25).map((i) => ({
+            x: i,
+            y: 0,
+            id: `${i}`,
+            amplificationPrimerId: null,
+            linearizationPrimerId: null,
+            homologyArmPrimerId: null,
+        }))
+        const plateDiagram = makePlateDiagram('storage').wells(wells)
+        expect(plateDiagram.getWells()).toHaveLength(25)
     })
 
     it('should render a plate diagram', () => {
-        const plateDiagram = makePlateDiagram({x: 5, y: 5})
+        const plateDiagram = makePlateDiagram('storage')
         const container = document.createElement('div')
         const plateDiagramRendered = plateDiagram.render(container)
-        expect(plateDiagramRendered.wells()).toHaveLength(25)
+        expect(container.querySelector('.plate-diagram-nodes')).not.toBeNull()
     })
 })
