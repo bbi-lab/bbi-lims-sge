95da184ea39b972c0511844c24414a2090bd33f2
David Reinhart <davidrr@uw.edu>
2025-04-18 13:09:32 -0700

    Update plate SQL view to include values from enum lookup object via CTE

--- server/db/schema/sge/plate.ts
+++ server/db/schema/sge/plate.ts
@@ -17,6 +17,13 @@ export const plates = pgTable('plates', {
 })
 
 const qb = new QueryBuilder()
+
+// create a CTE for plate types from ENUM_LOOKUPSto use in view
+const plateTypesAsSqlValues = _.map(ENUM_LOOKUPS.plates.plateType, (value, key) => {
+  return `('${key}', '${value.label}', '${value.desc}')`
+})
+const plateTypesCte = `with plate_types(plate_type_value, plate_type_label, plate_type_desc) AS (VALUES ${plateTypesAsSqlValues.join(', ')})`
+
 export const viewPlatesWithWellCounts = pgView('view_plates_with_well_counts', {
   id: uuid('id'),
   pcrExperimentId: uuid('pcr_experiment_id'),
@@ -24,21 +31,24 @@ export const viewPlatesWithWellCounts = pgView('view_plates_with_well_counts', {
   sizeX: smallint('size_x'),
   sizeY: smallint('size_y'),
   plateType: varchar('plate_type'),
+  plateTypeLabel: varchar('plate_type_label'),
   wellsCount: smallint('wells_count'),
   wellsWithContentCount: smallint('wells_with_content_count'),
-}).as(sql`select
+}).as(sql`${sql.raw(plateTypesCte)} select
     ${plates.id},
     ${plates.pcrExperimentId},
     ${plates.name},
     ${plates.sizeX},
     ${plates.sizeY},
     ${plates.plateType},
+    (select distinct on (plate_type_value) plate_type_label from plate_types where plate_type_value = ${plates.plateType}) as plate_type_label,
     count(${wells.id}) as wells_count,
     count(${wellContents.wellId}) as wells_with_content_count
-    from ${plates} join ${wells} on ${eq(plates.id, wells.plateId)}
-    left join ${wellContents} on ${eq(wells.id, wellContents.wellId)} group by ${plates.id}`
+    from ${plates}
+    join ${wells} on ${eq(plates.id, wells.plateId)}
+    left join ${wellContents} on ${eq(wells.id, wellContents.wellId)}
+    group by ${plates.id}`
 )
-
 
 const selectPlateSchema = createSelectSchema(plates)
 const insertPlateSchema = selectPlateSchema.omit({id: true, sizeX: true, sizeY: true})
