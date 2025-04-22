d1aea7cd18b432f53b9ed2d8d012a8f2efebfd29
David Reinhart <davidrr@uw.edu>
2025-04-22 09:54:39 -0700

    Rework extraction experiment page based on sample data and additional details provided

--- pages/sge/extraction-experiment/[id]/extraction.vue
+++ pages/sge/extraction-experiment/[id]/extraction.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import _ from 'lodash'
 import { RecordService } from '~/utils/service/RecordService'
+import { VALID_PROTOCOLS, type NucleicAcid } from '~/server/db/schema/sge/nucleic-acid'
 
-const nucleicAcidsToAdd = ref<any[]>([])
+
 const extractionExperiment = ref()
 const pelletsTable = ref()
+const nucleicAcidsTable = ref()
+const showEditForm = ref(false)
+const showMultipleEditForm = ref(false)
+const showNucleicAcidEditDialog = computed(() => {
+    return showEditForm.value || showMultipleEditForm.value
+})
+const editingRecordId = ref<string | undefined>()
+const editingMultipleRecordsIds = ref<string[]>([])
 
 const route = useRoute()
 const config = useRuntimeConfig()
@@ -12,43 +21,89 @@ const toast = useToast()
 
 onMounted(async() => {
     const extractionExperimentId = route.params.id as string
-    extractionExperiment.value = await RecordService.getRecord(`${config.public.apiBase}/extraction-experiments`, extractionExperimentId)
+    extractionExperiment.value = await RecordService.getRecord(`${config.public.apiBase}/extraction-experiments`, extractionExperimentId, {})
 })
 
-const submitNucleicAcids = () => {
-    // console.log(nucleicAcidsToAdd.value)
-}
-const addPelletsToExperiment = () => {
-    const pelletsSkipped: any[] = []
-    const pelletsToAdd: any[] = []
-    _.forEach(pelletsTable?.value?.selectedRecords || [], (pellet) => {
-        if (!_.includes(_.map(nucleicAcidsToAdd.value || [], (x) => x.id), pellet.id)) {
-            pelletsToAdd.push(pellet)
-        } else {
-            pelletsSkipped.push(pellet)
+const extractFromSelectedPellets = async () => {
+    const nucleicAcidsToAdd = _.map(pelletsTable.value.selectedRecords, (pellet) => {
+        return {
+            pelletId: pellet.id,
+            extractionExperimentId: route.params.id,
         }
     })
-
-    nucleicAcidsToAdd.value.push(...pelletsToAdd)
-    if (pelletsToAdd.length) {
+    try {
+        const nucleicAcidsAdded = await RecordService.addRecords(`${config.public.apiBase}/nucleic-acids`, nucleicAcidsToAdd) as NucleicAcid[]
+        if (!_.isEmpty(nucleicAcidsAdded)) {
+            toast.add({
+                severity: 'success',
+                summary: 'Nucleic acids added',
+                detail: `${_.size(nucleicAcidsAdded)} nucleic acids added`,
+                life: 3000,
+            })
+            _.forEach(nucleicAcidsAdded, (x) => {
+                nucleicAcidsTable.value.addOrRefreshRecordId(x.id)
+                pelletsTable.value.removeRecordId(x.pelletId)
+            })
+            pelletsTable.value.selectedRecords = []
+        } else {
+            toast.add({
+                severity: 'error',
+                summary: 'Error adding nucleic acids',
+                detail: `Failed to add nucleic acids`,
+                life: 3000,
+            })
+        }
+    } catch (e: any) {
         toast.add({
-            severity: 'success',
-            summary: 'Pellets added',
-            detail: `${_.size(pelletsToAdd)} pellets added to the extraction experiment`,
-            life: 3000,
-        })
-    }
-    if (pelletsSkipped.length) {
-        toast.add({
-            severity: 'warn',
-            summary: 'Pellets skipped',
-            detail: `${_.size(pelletsSkipped)} pellets already in the extraction experiment`,
+            severity: 'error',
+            summary: 'Error adding nucleic acids',
+            detail: e.statusMessage || 'Unknown error',
             life: 3000,
         })
     }
 }
 
-const displayWithClause = Object.freeze({
+const didClickRecordEdit = (event: any) => {
+    editingRecordId.value = event.id
+    showEditForm.value = true
+    showMultipleEditForm.value = false
+}
+
+const didClickMultipleRecordEdit = (recordIds: string[]) => {
+    editingMultipleRecordsIds.value = recordIds
+    showMultipleEditForm.value = true
+    showEditForm.value = false
+}
+
+const didClickCancelEditForm = () => {
+    editingRecordId.value = undefined
+    showEditForm.value = false
+}
+const didClickCancelMultipleEditForm = () => {
+    editingMultipleRecordsIds.value = []
+    showMultipleEditForm.value = false
+}
+const didUpdateRecord = (event: any) => {
+    if (event.id) nucleicAcidsTable.value.addOrRefreshRecordId(event.id)
+    showEditForm.value = false
+}
+const didUpdateMultipleRecords = (event: any[]) => {
+    event.forEach(e => {
+        if (e.id) nucleicAcidsTable.value.addOrRefreshRecordId(e.id)
+    })
+    showMultipleEditForm.value = false
+}
+const didDeleteRecord = (event: any) => {
+    if (event.id) nucleicAcidsTable.value.removeRecordId(event.id)
+    pelletsTable.value.addOrRefreshRecordId(event.pelletId)
+    showEditForm.value = false
+}
+const didDeleteMultipleNucleicAcids = (event: any[]) => {
+    event.forEach(e => {
+        pelletsTable.value.addOrRefreshRecordId(e.pelletId)
+    })
+}
+const pelletsWithClause = Object.freeze({
     transfectTarget: {
         columns: {},
         with: {
@@ -79,8 +134,16 @@ const displayWithClause = Object.freeze({
             }
         }
     },
+    nucleicAcid: {
+        columns: {
+            id: true
+        }
+    },
 })
-const columnDefs = {
+const pelletsColumnDefs = {
+    name: {
+        index: 0,
+    },
     transfectTarget: {
         header: 'Target',
         format: (x: any) => { return _.get(x, 'transfectTarget.target.name') || `${_.get(x, 'transfectTarget.target.region.gene.symbol')} : ${_.get(x, 'transfectTarget.target.region.name')}`},
@@ -95,72 +158,140 @@ const columnDefs = {
     storageBoxId: {display: false},
     storageBoxLoc: {display: false},
     d3Confluency: {display: false},
-    dnaConcentration: {display: false},
-    dnaVolume: {display: false},
-    dnaYield: {display: false},
-    rnaConcentration: {display: false},
-    rnaVolume: {display: false},
-    rnaYield: {display: false},
+}
+
+const nucleicAcidsWithClause = {
+    pellet: {
+        columns: {
+            name: true,
+        }
+    }
+}
+const nucleicAcidsColumnDefs = {
+    name: {
+        path: 'pellet.name',
+        index: 0,
+    },
+    protocol: {
+        index: 1,
+    },
+    extractionExperimentId: {
+        display: false,
+    },
+    pelletId: {
+        display: false,
+    },
+    storageBoxId: {
+        display: false,
+    },
+    storageBoxLoc: {
+        display: false,
+    },
+}
+const nucleicAcidFieldDefs = {
+    extractionExperimentId: {
+        display: false,
+    },
+    storageBoxId: {
+        label: 'Storage box',
+        component: 'AutoCompleter',
+        props: {
+            searchBaseUrl: `${config.public.apiBase}/storage-boxes`,
+            searchFields: ['name'],
+            valueField: 'id',
+            displayFields: ['name'],
+        }
+    },
+    pelletId: {
+        label: 'Pellet',
+        component: 'AutoCompleter',
+        props: {
+            searchBaseUrl: `${config.public.apiBase}/pellets`,
+            searchFields: ['transfectTarget.experiment.name', 'transfectTarget.target.name'],
+            searchWithClause: {
+                transfectTarget: {columns: {}, with: {experiment: {columns: {name: true}}, target: {columns: {name: true}}}},
+            },
+            valueField: 'id',
+            displayFields: ['transfectTarget.experiment.name', 'transfectTarget.target.name'],
+        }
+    },
 }
 </script>
 <template>
     <div class="flex flex-col h-full overflow-y-hidden">
-        <div class="flex flex-row w-full p-5">
-            <h4 class="flex-none">Extraction experiment: {{ extractionExperiment?.name }} </h4>
-            <div class="flex-grow"></div>
-            <Button
-                size="large"
-                icon="pi pi-arrow-right"
-                iconPos="right"
-                severity="info"
-                class="flex-none"
-                label="Add selected pellets"
-                :disabled="!pelletsTable?.selectedRecords?.length"
-                @click="addPelletsToExperiment" />
-            <div class="flex-grow"></div>
-            <Button
-                size="large"
-                icon="pi pi-bolt"
-                severity="warn"
-                class="flex-none"
-                :label="`Extract DNA`"
-                :disabled="!nucleicAcidsToAdd.length"
-                @click="submitNucleicAcids" />
+        <div class="flex w-full mt-5 ml-5">
+            <h4>Extraction experiment: {{ extractionExperiment?.name }} </h4>
         </div>
         <Splitter class="h-full overflow-y-hidden">
-            <SplitterPanel :size="60">
+            <SplitterPanel :size="50">
                 <QuickTable
                     ref="pelletsTable"
                     tableName="pellets"
                     title="Pellets"
                     schemaName="select"
-                    :columnDefs="columnDefs"
-                    :withClause="displayWithClause"
+                    :columnDefs="pelletsColumnDefs"
+                    :withClause="pelletsWithClause"
+                    :where="{'==':[{'var': 'nucleicAcid'}, null]}"
                     :canAdd="false"
                     :canDelete="false"
                     :canEdit="false"
                     :canExport="false"
+                    :hideSettings="true"
+                >
+                    <template #header-buttons>
+                        <Button
+                            size="large"
+                            icon="pi pi-bolt"
+                            iconPos="right"
+                            severity="warn"
+                            class="flex-none"
+                            label="Extract"
+                            :disabled="!pelletsTable?.selectedRecords?.length"
+                            @click="extractFromSelectedPellets" />
+                    </template>
+                </QuickTable>
+            </SplitterPanel>
+            <SplitterPanel :size="50" :minSize="25">
+                <QuickTable
+                    ref="nucleicAcidsTable"
+                    tableName="nucleic-acids"
+                    title="Nucleic acids"
+                    schemaName="select"
+                    :where="{'==':[{'var': 'extractionExperimentId'}, route.params.id]}"
+                    :withClause="nucleicAcidsWithClause"
+                    :columnDefs="nucleicAcidsColumnDefs"
+                    :canAdd="false"
+                    :canDelete="true"
+                    :canEdit="true"
+                    :canEditMultiple="true"
+                    :canExport="false"
+                    :hideSettings="true"
+                    @clickedRecordEdit="didClickRecordEdit"
+                    @clickedMultipleRecordEdit="didClickMultipleRecordEdit"
+                    @didDeleteMultipleRecords="didDeleteMultipleNucleicAcids"
                 />
             </SplitterPanel>
-            <SplitterPanel :size="40" :minSize="25">
-                <DataTable
-                    :value="nucleicAcidsToAdd"
-                    dataKey="id"
-                    tableStyle="min-width: 50rem"
-                    :scrollable="true"
-                    scrollHeight="flex">
-                    <template #header>
-                        <span class="text-xl font-bold">Pellets to process</span>
-                    </template>
-                    <template #empty> No data </template>
-                    <Column field="transfectTarget.target.name" header="target"></Column>
-                    <Column field="replicates" header="Replicates">
-                        <template #body="slotProps">
-                            {{ _.join(slotProps.data.replicates, ', ') }}
-                        </template>
-                    </Column>
-                </DataTable>
-            </SplitterPanel>
         </Splitter>
+        <Dialog v-model:visible="showNucleicAcidEditDialog" modal header="Edit" :style="{ width: 'auto' }" :closable="false">
+            <QuickForm
+                v-if="showEditForm"
+                :recordId="editingRecordId"
+                tableName="nucleicAcids"
+                schemaName="update"
+                :fieldDefs="nucleicAcidFieldDefs"
+                @cancel="didClickCancelEditForm"
+                @recordUpdate="didUpdateRecord"
+                @recordDelete="didDeleteRecord"
+            />
+            <QuickFormMultiple
+                v-if="showMultipleEditForm"
+                tableName="nucleic-acids"
+                :recordIds="editingMultipleRecordsIds"
+                schemaName="update"
+                :fieldDefs="nucleicAcidFieldDefs"
+                @cancel="didClickCancelMultipleEditForm"
+                @records-update="didUpdateMultipleRecords"
+            />
+        </Dialog>
     </div>
 </template>
