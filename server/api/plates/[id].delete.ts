0c5e5b871f32a7cc9b0c1d4b1febb0abfcebe9a5
David Reinhart <davidrr@uw.edu>
2025-05-12 09:10:52 -0700

    Fix API endpoint to allow deletion of empty plates

--- server/api/plates/[id].delete.ts
+++ server/api/plates/[id].delete.ts
@@ -1,18 +1,18 @@
 import { deleteRecord } from '~/server/services/generic-services'
 import { eq, or, and, isNotNull } from 'drizzle-orm'
 import { plates } from '~/server/db/schema/sge/plate'
-import { wells } from '~/server/db/schema/sge/well'
+import { wellContents, wells } from '~/server/db/schema/sge/well'
 
 export default defineEventHandler(async (event) => {
     const { id } = event.context.params as {id: string}
 
     try {
-        const nonEmptyWells = await db.select().from(wells).where(
-            and(
-                eq(wells.plateId, id),
-                or(isNotNull(wells.amplificationPrimerId), isNotNull(wells.homologyArmPrimerId), isNotNull(wells.linearizationPrimerId))
-            )
-        )
+
+        const nonEmptyWells = await db.select()
+            .from(wells)
+            .innerJoin(wellContents, eq(wells.id, wellContents.wellId))
+            .where(eq(wells.plateId, id))
+
         // throw error if wells are not empty
         if (nonEmptyWells.length > 0) {
             throw createError({
