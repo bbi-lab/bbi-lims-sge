1760b72d8597e3c59ac3855891e492463be7c812
David Reinhart <davidrr@uw.edu>
2025-05-12 09:24:27 -0700

    Automatically add associated plate when new PCR experiment is created

--- pages/sge/pcr-experiments.vue
+++ pages/sge/pcr-experiments.vue
@@ -3,6 +3,7 @@
 import _ from 'lodash'
 import type { ColumnDefinitions } from '~/components/QuickTable.client.vue'
 import { ENUM_LOOKUPS } from '~/server/db/schema/sge/enum-lookups'
+import { RecordService } from '~/utils/service/RecordService'
 import PhGridNineFill from '~icons/ph/grid-nine-fill'
 
 const showAddForm = ref(false)
@@ -10,6 +11,7 @@ const showEditForm = ref(false)
 const editingRecordId = ref<string | null>(null)
 const pcrExperimentsTable = ref()
 const router = useRouter()
+const config = useRuntimeConfig()
 
 function didClickRecordEdit(event: any) {
     editingRecordId.value = event.id
@@ -29,7 +31,15 @@ function didClickCancelEditForm() {
     showEditForm.value = false
 }
 
-function didAddRecord(event: any) {
+async function didAddRecord(event: any) {
+    // add corresponding plate
+    await RecordService.addRecord(`${config.public.apiBase}/plates`, {
+        name: event.name,
+        sizeX: 12,
+        sizeY: 8,
+        plateType: event.pcrType,
+        pcrExperimentId: event.id,
+    })
     pcrExperimentsTable.value.addOrRefreshRecordId(event.id)
     showAddForm.value = false
 }
@@ -53,6 +63,9 @@ const columnDefs: ColumnDefinitions = {
         path: 'technician.name',
     },
     pcrType: {
+        display: false,
+    },
+    pcrTypeLabel: {
         header: 'Type',
         format: (x: any) => {
             return _.get(ENUM_LOOKUPS.pcrExperiments.pcrType, [x.pcrType, 'label'])
@@ -64,7 +77,7 @@ const rowActions = {
     plates: {
         label: (data: any) => { return `${data.plates?.length || 0}`},  // for this to work, we need to expand plates
         action: (data: any) => {
-            router.push({path:'/sge/plates', query: {'pcrExperimentId': data.id}})
+            router.push({path:`/sge/plate-diagram/${data.pcrType}/${data.plates[0].id}`})
         },
         iconComponent: PhGridNineFill,
         iconPos: 'right',
@@ -81,7 +94,7 @@ const rowActions = {
                 schemaName="select"
                 title="PCR Experiments"
                 :rowActions="rowActions"
-                :withClause="{plates: true, technician: {columns: {name: true}}}"
+                :withClause="{plates: {columns: {id: true}}, technician: {columns: {name: true}}}"
                 :columnDefs="columnDefs"
                 @clickedRecordEdit="didClickRecordEdit"
                 @clickedRecordAdd="didClickRecordAdd"
