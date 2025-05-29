a9a61fc580da6ec6c261cff1d0c7667e7d65ed81
David Reinhart <davidrr@uw.edu>
2025-05-29 15:10:09 -0700

    Update well contents delete endpoint to delete associated well content source records

--- server/api/well-contents/[id].delete.ts
+++ server/api/well-contents/[id].delete.ts
@@ -1,13 +1,21 @@
-import { deleteRecord } from '~/server/services/generic-services'
 import _ from 'lodash'
-import { wellContents } from '~/server/db/schema/sge/well'
+import { wellContents, wellContentSources } from '~/server/db/schema/sge/well'
+import { eq } from 'drizzle-orm'
+import { deleteRecord } from '~/server/services/generic-services'
 
 export default defineEventHandler(async (event) => {
     const { id } = event.context.params as {id: string}
 
     try {
-        const deletedRecord = await deleteRecord(wellContents, id)
-        return deletedRecord
+        // delete well content and sources, wrap in single transaction to prevent partial deletion
+        const transactionResult = await db.transaction(async (tx) => {
+            await db.delete(wellContentSources).where(eq(wellContentSources.wellContentId, id))
+            const deletedRecord = await deleteRecord(wellContents, id)
+
+            return deletedRecord
+        })
+
+        return transactionResult
     } catch (e: any) {
         throw createError({
             statusCode: 400,
