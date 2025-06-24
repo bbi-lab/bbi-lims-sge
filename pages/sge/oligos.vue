8ec0b1d4a49acfb60001abb09e404494cf62f5ec
David Reinhart <davidrr@uw.edu>
2025-06-23 20:59:23 -0700

    Add oligos data entry page

---
+++ pages/sge/oligos.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import _ from 'lodash'
+
+const crudTable = useCrudTable()
+const config = useRuntimeConfig()
+
+const rowActions = {}
+const columnDefs = {
+    targetId: {
+        header: 'Target',
+        format: (x: any) => {
+            return x.target?.name || (x.target?.region ? `${_.get(x, 'target.region.gene.symbol')} : ${_.get(x, 'target.region.name')}` : '')
+        },
+        path: 'targetId.displayValue',
+        type: 'string',
+        index: 2,
+    },
+    wellContents: {display: false},
+}
+const fieldDefs = {
+    targetId: {
+        label: 'Target',
+        component: 'AutoCompleter',
+        props: {
+            searchBaseUrl: `${config.public.apiBase}/targets`,
+            searchFields: ['name'],
+            valueField: 'id',
+            displayFields: ['name'],
+            dropdown: true,
+        }
+    },
+}
+const displayWithClause = {
+    target: {
+        columns: {
+            name: true
+        },
+        with: {
+            project: {
+                columns: {
+                    name: true
+                }
+            },
+            region: {
+                columns: {
+                    name: true
+                },
+                with: {
+                    gene: {
+                        columns: {
+                            symbol: true
+                        }
+                    }
+                }
+            },
+        },
+    },
+    wellContents: {
+        with: {
+            well: {
+                columns: {
+                    id: true,
+                    x: true,
+                    y: true,
+                },
+                with: {
+                    plate: {
+                        columns: {
+                            id: true,
+                            name: true,
+                            plateType: true,
+                        }
+                    }
+                }
+            },
+        },
+    },
+}
+</script>
+<template>
+    <Splitter class="h-full overflow-y-hidden">
+        <SplitterPanel :size="50">
+            <QuickTable
+                :ref="crudTable.setTableRef"
+                tableName="oligos"
+                schemaName="select"
+                title="Oligos"
+                :rowActions="rowActions"
+                :columnDefs="columnDefs"
+                :withClause="displayWithClause"
+                :selectionDisabled="crudTable.state.showAddForm || crudTable.state.showEditForm"
+                @clickedRecordEdit="crudTable.didClickRecordEdit"
+                @clickedRecordAdd="crudTable.didClickRecordAdd"
+            />
+        </SplitterPanel>
+         <SplitterPanel v-if="crudTable.state.showAddForm || crudTable.state.showEditForm">
+            <QuickForm
+                v-if="crudTable.state.showAddForm"
+                tableName="oligos"
+                schemaName="insert"
+                :fieldDefs="fieldDefs"
+                @cancel="crudTable.didClickCancelAddForm"
+                @recordAdd="crudTable.didAddRecord"
+            />
+            <QuickForm
+                v-if="crudTable.state.editingRecordId && crudTable.state.showEditForm"
+                tableName="oligos"
+                schemaName="update"
+                :recordId="crudTable.state.editingRecordId"
+                :fieldDefs="fieldDefs"
+                @cancel="crudTable.didClickCancelEditForm"
+                @recordUpdate="crudTable.didUpdateRecord"
+            />
+        </SplitterPanel>
+    </Splitter>
+</template>
