92282ba9ed1a59783dd49c7a19ed2b17d027c3d4
David Reinhart <davidrr@uw.edu>
2025-06-27 15:51:39 -0700

    Update transfection target table to remove calculated fields and add foreign keys to plasmids table

--- server/db/schema/sge/transfect-experiment.ts
+++ server/db/schema/sge/transfect-experiment.ts
@@ -3,6 +3,7 @@ import { users } from '../user'
 import { targets } from './target'
 import { lots } from './lots'
 import { cycles } from './cycle'
+import { plasmids } from './plasmid'
 
 export const transfectExperiments = pgTable('transfect_experiments', {
   id: uuid('id').notNull().primaryKey().defaultRandom(),
@@ -11,7 +12,7 @@ export const transfectExperiments = pgTable('transfect_experiments', {
   startedOn: timestamp('started_on').notNull().defaultNow(),
   transfectionCount: integer('transfection_count'),
   replicateCount: integer('replicates_count').notNull(),
-  negativeControl: boolean('negative_control'),
+  // negativeControl: boolean('negative_control'),
 })
 
 export const transfectTargets = pgTable('transfect_targets', {
@@ -19,18 +20,21 @@ export const transfectTargets = pgTable('transfect_targets', {
   experimentId: uuid('experiment_id').references(() => transfectExperiments.id).notNull(),
   targetId: uuid('target_id').references(() => targets.id).notNull(),
   snvLibraryConc: doublePrecision('snv_library_conc'),
-  snvLibraryTo3ugVol: doublePrecision('snv_library_to_3ug_vol'),
-  sgRna: varchar('sg_rna', { length: 255 }),
+  snvLib: uuid('snv_lib').references(() => plasmids.id),
+  // snvLibraryTo3ugVol: doublePrecision('snv_library_to_3ug_vol'),
+  // sgRna: varchar('name', { length: 255 }),
   sgRnaConc: doublePrecision('sg_rna_conc'),
-  sgRnaTo12ugVol: doublePrecision('sg_rna_to_12ug_vol'),
+  sgRna: uuid('sg_rna').references(() => plasmids.id),
+  // sgRnaTo12ugVol: doublePrecision('sg_rna_to_12ug_vol'),
   sgRnaNegControl: varchar('sg_rna_neg_control', { length: 255 }),
   hprt1SgRnaConc: doublePrecision('hprt1_sg_rna_conc'),
-  hprt1SgRnaTo12ugVol: doublePrecision('hprt1_sg_rna_to_12ug_vol'),
+  // hprt1SgRnaTo12ugVol: doublePrecision('hprt1_sg_rna_to_12ug_vol'),
   xfectBuffer: doublePrecision('xfect_buffer'),
   xfectPolymerPerTransfect: doublePrecision('xfect_polymer_per_transfect'),
   transfectionCount: integer('transfection_count').notNull(),
-  snvLibNeeded: doublePrecision('snv_lib_needed'),
-  sgRnaNeeded: doublePrecision('sg_rna_needed'),
+  negativeControl: boolean('negative_control'),
+  // snvLibNeeded: doublePrecision('snv_lib_needed'),
+  // sgRnaNeeded: doublePrecision('sg_rna_needed'),
   notes: text('notes'),
 }, (t) => [
   unique('unique_transfect_experiment_target_count').on(t.experimentId, t.targetId, t.transfectionCount),
