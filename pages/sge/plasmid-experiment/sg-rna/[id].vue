f654970cb0252f17eec85170d14b0e53847519d5
David Reinhart <davidrr@uw.edu>
2025-07-11 15:47:51 -0700

    Update sgRNA cloning experiment page to allow for transferring of well contents from existing guide-rna plates to experiment plate

--- pages/sge/plasmid-experiment/sg-rna/[id].vue
+++ pages/sge/plasmid-experiment/sg-rna/[id].vue
@@ -1,141 +1,149 @@
 <script setup lang="ts">
 import _ from 'lodash'
+import { wellCoordinateToChar } from '~/lib/plate-diagram'
+import type { User } from '~/server/db/schema/user';
 import { RecordService } from '~/utils/service/RecordService'
+import IxMoveLayerDown from '~icons/ix/move-layer-down'
 
 const { breakpoints } = useLayout()
 const route = useRoute()
-const router = useRouter()
 const plateLayout = usePlateLayout()
+const sourcePlateLayout = usePlateLayout()
+const sourcePlateWithWellSpecs = ref()
 const toast = useToast()
+const { user } = useUserSession()
+const config = useRuntimeConfig()
 
 const smallerThanLg = breakpoints.smaller('lg')
-const plasmidExperiment = ref()
-const config = useRuntimeConfig()
-const selectionTableName = ref<string>('view-plates-with-well-counts')
+const plateWithWellSpecs = ref()
 const selectionTableKey = ref(0)
+const sgRnaCloningExperiment = ref()
 
-onMounted(async() => {
-    nextTick(() => refreshExperiment())
+const selectedSourcePlate = computed(() => {
+    return plateLayout.selectionTableRef.value?.selectedRecords
 })
 
-const refreshExperiment = async () => {
-    plasmidExperiment.value = await RecordService.getRecord(`${config.public.apiBase}/plasmid-experiments`, route.params.id as string,
-    {
-        plates: true,
-        snvLibs: true,
-    })
-}
-const selectionTableOptions = [
-    { label: 'Guide RNA plates', value: 'view-plates-with-well-counts' },
-    { label: 'SNV library', value: 'plasmids' },
-]
-
-watch(selectionTableName, async (newValue) => {
-    selectionTableKey.value += 1
-})
-
-const whereClause = computed(() => {
-    if (selectionTableName.value === 'plasmids') {
-        return {
-            and: [
-                {"==": [{"var": "plasmidType"}, "library"]},
-                {"!=": [{"var": "plasmidExperimentId"}, route.params.id as string]},
-            ]
-        }
-    } else if (selectionTableName.value === 'view-plates-with-well-counts') {
-        return {
-            and: [
-                {"==": [{"var": "plateType"}, "guide-rna"]},
-                {"!=": [{"var": "plasmidExperimentId"}, route.params.id as string]},
-            ]
+watch (selectedSourcePlate, async (newValue) => {
+    if (newValue?.id) {
+        sourcePlateLayout.setPlateId(newValue.id)
+        await sourcePlateLayout.loadPlate({
+            oligo: true,
+        })
+        sourcePlateWithWellSpecs.value = {
+            ...sourcePlateLayout.plateWithWellContents.value,
+            wells: _.values(sourcePlateLayout.wellSpecs.value),
         }
     } else {
-        return {}
+        sourcePlateWithWellSpecs.value = null
     }
 })
 
-const frozenRecordIds = computed(() => {
-    return _.compact(_.flatten(_.map(plateLayout.selectedWells.value, 'selectionTableRecordIds')))
-})
-const columnDefs = computed(() => {
-    if (selectionTableName.value === 'view-plates-with-well-counts') {
-        return {
-            plateType: { display: false },
-            plateTypeLabel: { header: 'Type' },
-            cycleName: { display: false },
-            cycleId: { display: false },
-            targets: { display: false },
-            pcrExperimentId: { display: false},
-            plasmidExperimentId: { display: false},
-            discarded: { display: false},
-            processed: { display: false},
-            sizeX: { display: false },
-            sizeY: { display: false },
-            wellsCount: { display: false },
-            wellsWithContentCount: { display: false },
-            wellsProcessedCount: { display: false },
-            filled: {
-                format: (data: any) => {
-                    if (data.wellsCount - data.wellsWithContentCount) {
-                        return `${data.wellsWithContentCount} / ${data.wellsCount}`
-                    } else {
-                        return '-'
-                    }
-                },
-                path: 'filled.displayValue',
-            },
-        }
-    } else if (selectionTableName.value === 'plasmids') {
-        return {
-            targetId: { display: false },
-            plasmidExperimentId: { display: false },
-            wellContents: {display: false}
-        }
+onMounted(async() => {
+    sgRnaCloningExperiment.value = await RecordService.getRecord(
+        `${config.public.apiBase}/sg-rna-cloning-experiments`,
+        route.params.id as string,
+        {
+            plates: true
+        },
+    )
+
+    const plateId = _.get(sgRnaCloningExperiment.value, 'plates[0].id')
+
+    plateLayout.wellContentsDisplayConfig.value = {
+        colorBy: ['oligo.targetId'],
+        selectionTableRecordIdPaths: ['oligoId'],
+        tooltip: (well: any) => {
+            const wellCoordinate = `${wellCoordinateToChar(well.y)}${well.x}`
+            const oligos = _.map(well.wellContents, 'oligo')
+            return oligos ? `${wellCoordinate}:<br>` + _.map(oligos, 'name').join('<br>') : wellCoordinate
+        },
+        symbol: (well: any) => {
+            const oligos = _.compact(_.map(well.wellContents, 'oligo'))
+            return oligos ? _.size(oligos) : ''
+        },
     }
-    return {}
+    sourcePlateLayout.wellContentsDisplayConfig.value = _.clone(plateLayout.wellContentsDisplayConfig.value)
+
+    if (plateId) {
+        plateLayout.setPlateId(plateId)
+        loadPlate()
+    }
 })
-const removePlasmidFromExperiment = async (id: string) => {
-    await RecordService.updateRecord(`${config.public.apiBase}/plasmids`, {
-            id,
-            plasmidExperimentId: null
-    })
-    refreshExperiment()
-    selectionTableKey.value += 1
-    toast.add({severity: 'success', summary: 'Success', detail: 'Removed from experiment', life: 3000})
+
+const loadPlate = async () => {
+    await plateLayout.loadPlate(
+        {
+            oligo: true,
+        },
+    )
+
+    plateWithWellSpecs.value = {
+        ...plateLayout.plateWithWellContents.value,
+        wells: _.values(plateLayout.wellSpecs.value),
+    }
 }
-const removePlateFromExperiment = async (id: string) => {
-    await RecordService.updateRecord(`${config.public.apiBase}/plates`, {
-            id,
-            plasmidExperimentId: null
-    })
-    refreshExperiment()
-    selectionTableKey.value += 1
-    toast.add({severity: 'success', summary: 'Success', detail: 'Removed from experiment', life: 3000})
+
+const transferSelectedWellsContents = async () => {
+    const sourceWells = sourcePlateLayout!.selectedWells.value
+    const destinationWells = plateLayout.selectedWells.value
+
+    if (_.isEmpty(sourceWells)) {
+        toast.add({severity: 'warn', summary: 'No wells selected for transfer', life: 3000})
+    } else if (sourceWells.length !== destinationWells.length) {
+        toast.add({severity: 'warn', summary: 'Number of selected wells in source plate does not match number of selected wells in destination plate', life: 3000})
+    } else {
+        const sourceWellsSorted = _.sortBy(sourceWells, ['x', 'y'])
+        const destinationWellsSorted = _.sortBy(destinationWells, ['x', 'y'])
+
+        const wellContentsToAdd = _.flatten(_.map(sourceWellsSorted, (well, index) => {
+            const wellContents = well.data.wellContents
+            const destinationWell = destinationWellsSorted[index]
+            return _.map(wellContents, (wellContent) => {
+                return {
+                    ..._.pick(wellContent, ['oligoId']),
+                    wellId: destinationWell.id,
+                    sourceWellIds: [well.id],
+                    createdBy: (user.value as User)?.id,
+                }
+            })
+        }))
+        await plateLayout.addWellContents(wellContentsToAdd.flat())
+    }
 }
-const rowActions = {
-    assign: {
-        label: '',
-        action: async (data: any) => {
-            if (selectionTableName.value === 'view-plates-with-well-counts') {
-                await RecordService.updateRecord(`${config.public.apiBase}/plates`, {
-                    id: data.id,
-                    plasmidExperimentId: route.params.id as string
-                })
-                refreshExperiment()
-                selectionTableKey.value += 1
-            } else if (selectionTableName.value === 'plasmids') {
-                await RecordService.updateRecord(`${config.public.apiBase}/plasmids`, {
-                    id: data.id,
-                    plasmidExperimentId: route.params.id as string
-                })
-                refreshExperiment()
-                selectionTableKey.value += 1
+
+const columnDefs = {
+    plateType: { display: false },
+    snvLibCloningExperimentId: { display: false },
+    sgRnaCloningExperimentId: { display: false },
+    plateTypeLabel: { header: 'Type' },
+    cycleName: { display: false },
+    cycleId: { display: false },
+    targets: { display: false },
+    pcrExperimentId: { display: false},
+    plasmidExperimentId: { display: false},
+    sizeX: { display: false },
+    sizeY: { display: false },
+    wellsCount: { display: false },
+    wellsWithContentCount: { display: false },
+    wellsProcessedCount: { display: false },
+    filled: {
+        format: (data: any) => {
+            if (data.wellsCount - data.wellsWithContentCount) {
+                return `${data.wellsWithContentCount} / ${data.wellsCount}`
+            } else {
+                return '-'
             }
         },
-        icon: 'pi pi-fw pi-arrow-right',
-        iconPos: 'right',
-        tooltip: 'Add to experiment',
+        path: 'filled.displayValue',
     },
+}
+
+// for source plates table, only select guide RNA plates that are not associated with an experiment
+const whereClause ={
+    "and": [
+        {"in": [{"var": "plateType"}, ["guide-rna"]]},
+        {"==": [{"var": "sgRnaCloningExperimentId"}, null]},
+    ]
 }
 </script>
 <template>
@@ -144,7 +152,7 @@ const rowActions = {
             <QuickTable
                 :key="selectionTableKey"
                 :ref="plateLayout.setSelectionTableRef"
-                :tableName="selectionTableName"
+                tableName="view-plates-with-well-counts"
                 schemaName="select"
                 :canAdd="false"
                 :canDelete="false"
@@ -152,70 +160,71 @@ const rowActions = {
                 :canExport="false"
                 :where="whereClause"
                 :columnDefs="columnDefs"
-                :rowActions="rowActions"
-                :showColumnFilters="true"
+                :sortBy="['name']"
                 selectionMode="single"
+                :showColumnFilters="true"
                 emptyMessage="">
-                <template #header-buttons>
-                    <SelectButton v-model="selectionTableName" :options="selectionTableOptions" optionLabel="label" optionValue="value" dataKey="label" />
-                </template>
             </QuickTable>
         </SplitterPanel>
-        <SplitterPanel class="overflow-scroll mt-10 ml-5" :size="40" :minSize="25">
-            <template v-if="plasmidExperiment">
-                <div class="text-lg font-bold mb-2">
-                    sg-RNA cloning: {{ plasmidExperiment.name }}
-                </div>
-                <hr/>
-                <div class="font-bold mb-2">
-                    Plates:
-                </div>
-                <div v-for="plate of plasmidExperiment.plates" class="mb-2">
-                    {{ plate.name }}
-                    <Button
-                        class="ml-2"
-                        severity="info"
-                        v-tooltip="{ value: 'Plate layout', showDelay: 300 }"
-                        @click="router.push({path:`/sge/plate-layout/guide-rna/${plate.id}`})"
-                    >
-                        <template #icon>
-                            <span class="p-button-icon-right inline-block">
-                               <PhGridNineFill />
-                            </span>
+        <SplitterPanel :size="40" :minSize="25">
+            <Splitter layout="vertical">
+                <SplitterPanel class="flex justify-center overflow-scroll mt-10">
+                    <PlateDiagram
+                        :ref="sourcePlateLayout?.setPlateDiagramRef"
+                        v-if="selectedSourcePlate?.id && sourcePlateWithWellSpecs"
+                        v-model="sourcePlateWithWellSpecs"
+                        :plateType="sourcePlateWithWellSpecs.plateType"
+                        :sizeX="sourcePlateWithWellSpecs.sizeX"
+                        :sizeY="sourcePlateWithWellSpecs.sizeY"
+                        @well-range-selected="sourcePlateLayout?.wellRangeSelected"
+                        @well-selection-cleared="sourcePlateLayout?.wellSelectionCleared"
+                        @all-wells-selected="sourcePlateLayout?.selectedAllWells"
+                        @well-contents-updated="sourcePlateLayout?.updatedWellContents" >
+                        <template #header>
+                            {{ sourcePlateWithWellSpecs.name }}
                         </template>
-                    </Button>
-                    <Button
-                        class="ml-2"
-                        icon="pi pi-times"
-                        severity="danger"
-                        outlined
-                        v-tooltip="{ value: 'Remove from experiment', showDelay: 300 }"
-                        @click="removePlateFromExperiment(plate.id)" />
-                </div>
-                <hr/>
-                <div class="font-bold mb-2">
-                    SNV-lib:
-                </div>
-                <div v-for="snvLib of plasmidExperiment.snvLibs" class="mb-2">
-                    {{ snvLib.name }}
-                    <Button
-                        as="a"
-                        class="ml-2"
-                        severity="info"
-                        icon="pi pi-spinner"
-                        v-tooltip="{ value: 'Plasmid', showDelay: 300 }"
-                        :href="`/sge/plasmids?id=${snvLib.id}`"
-                    >
-                    </Button>
-                    <Button
-                        class="ml-2"
-                        icon="pi pi-times"
-                        severity="danger"
-                        outlined
-                        v-tooltip="{ value: 'Remove from experiment', showDelay: 300 }"
-                        @click="removePlasmidFromExperiment(snvLib.id)" />
-                </div>
-            </template>
+                        <template #button1>
+                            <Button
+                                severity="secondary"
+                                v-tooltip="{value: 'Transfer well contents to PreSeq 3 plate', showDelay: 500}"
+                                :disabled="_.isEmpty(sourcePlateLayout?.selectedWells.value)"
+                                @click="transferSelectedWellsContents">
+                                <template #icon>
+                                    <IxMoveLayerDown />
+                                </template>
+                            </Button>
+                        </template>
+                    </PlateDiagram>
+                    <div v-else>
+                        <span class="text-gray-500">No source plate selected</span>
+                    </div>
+                </SplitterPanel>
+                <SplitterPanel class="flex justify-center overflow-scroll mt-10">
+                    <PlateDiagram
+                        :ref="plateLayout.setPlateDiagramRef"
+                        v-if="plateWithWellSpecs"
+                        v-model="plateWithWellSpecs"
+                        :plateType="plateWithWellSpecs.plateType"
+                        :sizeX="plateWithWellSpecs.sizeX"
+                        :sizeY="plateWithWellSpecs.sizeY"
+                        @well-range-selected="plateLayout.wellRangeSelected"
+                        @well-selection-cleared="plateLayout.wellSelectionCleared"
+                        @all-wells-selected="plateLayout.selectedAllWells"
+                        @well-contents-updated="plateLayout.updatedWellContents" >
+                        <template #header>
+                            {{ plateWithWellSpecs.name }}
+                        </template>
+                        <template #button1>
+                            <Button
+                                class="p-button-secondary"
+                                icon="pi pi-trash"
+                                v-tooltip="{value: 'Empty selected wells', showDelay: 500}"
+                                :disabled="_.isEmpty(plateLayout.selectedWells.value)"
+                                @click="plateLayout.emptySelectedWells" />
+                        </template>
+                    </PlateDiagram>
+                </SplitterPanel>
+            </Splitter>
         </SplitterPanel>
     </Splitter>
 </template>
