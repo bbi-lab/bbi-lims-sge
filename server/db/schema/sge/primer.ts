d6bd4467ccd4dbeb7b2c61833d238dbe14e9e752
David Reinhart <davidrr@uw.edu>
2025-08-08 13:13:50 -0700

    Add PCR 1 and 2 primers tables to db

--- server/db/schema/sge/primer.ts
+++ server/db/schema/sge/primer.ts
@@ -38,6 +38,30 @@ export const homologyArmPrimers = pgTable('homology_arm_primers', {
   check("sequence_check", sql`${table.sequence} ~* '^[actg]*$'`),
 ])
 
+export const pcr1Primers = pgTable('pcr_1_primers', {
+    id: uuid('id').notNull().primaryKey().defaultRandom(),
+    targetId: uuid('target_id').references(() => targets.id),
+    name: varchar('name', { length: 255 }).notNull(),
+    sequence: varchar('sequence', { length: 255 }),
+    sequenceType: varchar('sequence_type', {enum: ['forward', 'reverse']}),
+    notes: text('notes'),
+}, (table) => [
+  check("sequence_check", sql`${table.sequence} ~* '^[actg]*$'`),
+])
+
+export const pcr2Primers = pgTable('pcr_2_primers', {
+    id: uuid('id').notNull().primaryKey().defaultRandom(),
+    targetId: uuid('target_id').references(() => targets.id),
+    name: varchar('name', { length: 255 }).notNull(),
+    sequence: varchar('sequence', { length: 255 }),
+    sequenceType: varchar('sequence_type', {enum: ['forward', 'reverse']}),
+    adapterSequence: varchar('adapter_sequence', { length: 255 }),
+    notes: text('notes'),
+}, (table) => [
+  check("sequence_check", sql`${table.sequence} ~* '^[actg]*$'`),
+  check("adapter_sequence_check", sql`${table.adapterSequence} ~* '^[actg]*$'`),
+])
+
 export const indexPrimers = pgTable('index_primers', {
     id: uuid('id').notNull().primaryKey().defaultRandom(),
     name: varchar('name', { length: 255 }).notNull(),
