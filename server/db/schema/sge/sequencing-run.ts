1a4f40d8c6d9c150495437ddb76ab513d889c82a
David Reinhart <davidrr@uw.edu>
2025-08-14 14:07:46 -0700

    Update DB tables and view for sequencing run samples based on feedback
    - Dropping override_cycles column from external samples, converting to a calculated column in the related SQL view to be calculated for internal and external samples
    - Allow external samples to use internal indexing primers
    - Add source well ID column to external samples, in preparation for new 'External indexing plate' type that will allow stamping of index primer plates onto external samples plate
    - Update view_sequencing_run_all_samples SQL VIEW to display related well and plate information for external samples when available
    - Set default for million reads required to 5

--- server/db/schema/sge/sequencing-run.ts
+++ server/db/schema/sge/sequencing-run.ts
@@ -15,16 +15,18 @@ export const sequencingRuns = pgTable('sequencing_runs', {
 
 export const sequencingRunSamples = pgTable('sequencing_run_samples', {
   id: uuid('id').notNull().primaryKey().defaultRandom(),
+  projectName: varchar('project_name', { length: 255 }),
   sequencingRunId: uuid('sequencing_run_id').references(() => sequencingRuns.id),
   nucleicAcidId: uuid('nucleic_acid_id').references(() => nucleicAcids.id).notNull(),
   indexPrimer1Id: uuid('index_primer_1_id').references(() => indexPrimers.id).notNull(),
   indexPrimer2Id: uuid('index_primer_2_id').references(() => indexPrimers.id).notNull(),
   sourceWellId: uuid('source_well_id').references(() => wells.id),
-  millionReadsRequired: doublePrecision('million_reads_required'),
+  millionReadsRequired: doublePrecision('million_reads_required').default(5),
   notes: text('notes'),
   createdAt: timestamp('created_at').defaultNow(),
 }, (t) => [
   uniqueIndex('unique_index_primers_per_sequencing_run').on(t.sequencingRunId, sql`least(${t.indexPrimer1Id}, ${t.indexPrimer2Id})`, sql`greatest(${t.indexPrimer1Id}, ${t.indexPrimer2Id})`),
+  sql`CHECK (${t.indexPrimer1Id} IS NOT NULL AND ${t.indexPrimer2Id} IS NOT NULL)`
 ])
 
 export const sequencingRunExternalSamples = pgTable('sequencing_run_external_samples', {
@@ -32,14 +34,21 @@ export const sequencingRunExternalSamples = pgTable('sequencing_run_external_sam
   projectName: varchar('project_name', { length: 255 }),
   sequencingRunId: uuid('sequencing_run_id').references(() => sequencingRuns.id),
   externalSampleId: varchar('external_sample_id', { length: 255 }).notNull(),
+  indexPrimer1Id: uuid('index_primer_1_id').references(() => indexPrimers.id),
+  indexPrimer2Id: uuid('index_primer_2_id').references(() => indexPrimers.id),
   customIndexSeq1: varchar('custom_index_seq_1', { length: 50 }).notNull(),
   customIndexSeq2: varchar('custom_index_seq_2', { length: 50 }),
-  millionReadsRequired: doublePrecision('million_reads_required'),
-  overrideCycles: varchar('override_cycles', { length: 10 }).array(),
+  sourceWellId: uuid('source_well_id').references(() => wells.id),
+  millionReadsRequired: doublePrecision('million_reads_required').default(5),
   notes: text('notes'),
   createdAt: timestamp('created_at').defaultNow(),
 }, (t) => [
   uniqueIndex('unique_custom_index_seqs_per_sequencing_run').on(t.sequencingRunId, sql`least(${t.customIndexSeq1}, ${t.customIndexSeq2})`, sql`greatest(${t.customIndexSeq1}, ${t.customIndexSeq2})`),
+  // check that either two internal index primers are present, or at least one custom index sequences is present, but not both types
+  sql`CHECK (
+    ((COALESCE(TRIM(${t.customIndexSeq1}), '') <> '' OR COALESCE(TRIM(${t.customIndexSeq2}), '') <> '')) AND (${t.indexPrimer1Id} IS NULL AND ${t.indexPrimer2Id} IS NULL)) OR
+    (${t.indexPrimer1Id} IS NOT NULL AND ${t.indexPrimer2Id} IS NOT NULL)
+  )`
 ])
 
 export const viewSequencingRunAllSamples = pgView('view_sequencing_run_all_samples', {
@@ -79,7 +88,13 @@ export const viewSequencingRunAllSamples = pgView('view_sequencing_run_all_sampl
   NULL AS custom_index_seq_1,
   NULL AS custom_index_seq_2,
   million_reads_required,
-  NULL AS override_cycles,
+  CASE
+    WHEN index_primer_1_id IS NOT NULL AND index_primer_2_id IS NOT NULL THEN 'Y151;I10;I10;Y151'
+    WHEN index_primer_1_id IS NOT NULL AND index_primer_2_id IS NULL THEN 'Y151;I10;N10;Y151'
+    WHEN index_primer_1_id IS NULL AND index_primer_2_id IS NOT NULL THEN 'Y151;N10;I10;Y151'
+  ELSE
+    NULL
+  END AS override_cycles,
   sequencing_run_samples.notes AS notes,
   created_at
   FROM sequencing_run_samples
@@ -91,23 +106,33 @@ export const viewSequencingRunAllSamples = pgView('view_sequencing_run_all_sampl
   JOIN plates ON wells.plate_id = plates.id
   UNION
   SELECT
-  id,
+  sequencing_run_external_samples.id as id,
   external_sample_id as sample_name,
   sequencing_run_id,
   'external' AS sample_type,
   NULL AS nucleic_acid_id,
-  NULL AS index_primer_1_id,
-  NULL AS index_primer_2_id,
-  NULL AS index_primer_1_label,
-  NULL AS index_primer_2_label,
-  NULL AS source_well_id,
-  NULL as source_well_x,
-  NULL as source_well_y,
-  NULL as source_plate_name,
+  index_primer_1_id,
+  index_primer_2_id,
+  primer1.index_sequence || ' (' || primer1.primer_type || ')' as index_primer_1_label,
+  primer2.index_sequence || ' (' || primer2.primer_type || ')' as index_primer_2_label,
+  source_well_id,
+  wells.x as source_well_x,
+  wells.y as source_well_y,
+  plates.name as source_plate_name,
   custom_index_seq_1,
   custom_index_seq_2,
   million_reads_required,
-  override_cycles,
+  CASE
+    WHEN (index_primer_1_id != NULL AND index_primer_2_id != NULL) OR (COALESCE(TRIM(custom_index_seq_1), '') <> '' AND COALESCE(TRIM(custom_index_seq_2), '') <> '') THEN 'Y151;I10;I10;Y151'
+    WHEN (index_primer_1_id != NULL AND index_primer_2_id = NULL) OR (COALESCE(TRIM(custom_index_seq_1), '') <> '' AND COALESCE(TRIM(custom_index_seq_2), '') = '') THEN 'Y151;I10;N10;Y151'
+    WHEN (index_primer_1_id = NULL AND index_primer_2_id != NULL) OR (COALESCE(TRIM(custom_index_seq_1), '') = '' AND COALESCE(TRIM(custom_index_seq_2), '') <> '') THEN 'Y151;N10;I10;Y151'
+  ELSE
+    NULL
+  END AS override_cycles,
   notes,
   created_at
-  FROM sequencing_run_external_samples`)
+  FROM sequencing_run_external_samples
+  LEFT JOIN index_primers AS primer1 ON sequencing_run_external_samples.index_primer_1_id = primer1.id
+  LEFT JOIN index_primers AS primer2 ON sequencing_run_external_samples.index_primer_2_id = primer2.id
+  LEFT JOIN wells ON sequencing_run_external_samples.source_well_id = wells.id
+  LEFT JOIN plates ON wells.plate_id = plates.id`)
