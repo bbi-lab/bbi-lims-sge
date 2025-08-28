e4c45f206280287d00297551e4559581a8cb75e1
David Reinhart <davidrr@uw.edu>
2025-08-28 11:14:28 -0700

    Update harvest UI to use crudTable composable and edit forms and integrate with plate diagram component

--- pages/sge/transfect-experiment/[id]/harvest.client.vue
+++ pages/sge/transfect-experiment/[id]/harvest.client.vue
@@ -3,12 +3,13 @@ import moment from 'moment'
 import _ from 'lodash'
 import  {
     TransfectionExperiment,
-    VALID_TRANSFECTIONS,
     type TranfectionExperimentPellet,
 } from '~/shared/sge/transfection-experiment'
-import { wellCoordinateToChar } from '~/lib/plate-diagram'
+import { getWellTextColor, wellCoordinateToChar } from '~/lib/plate-diagram'
 import { RecordService } from '~/utils/service/RecordService'
 import type { WellContent } from '~/server/db/schema/sge/well'
+import type { ColumnDefinitions } from '~/components/QuickTable.client.vue'
+import type { FieldDefinitions } from '~/components/QuickForm.vue'
 
 const { user } = useUserSession()
 const config = useRuntimeConfig()
@@ -26,6 +27,12 @@ const pelletPlateKey = ref(0) // used to force re-render of PlateDiagram
 const plateWithWellSpecs = ref()
 const plateLayout = usePlateLayout()
 const smallerThanLg = breakpoints.smaller('lg')
+const crudTable = useCrudTable()
+const crudTableKey = ref(0)
+
+const showPelletEditDialog = computed(() => {
+    return crudTable.state.showEditForm || crudTable.state.showMultipleEditForm
+})
 
 const allTargets = computed(() => {
     return currentHarvestDay.value != 5 ? [] : _.map(experiment.value?.transfectTargets, (x) => {return {label: `${x.target.name} (${x.transfectionCount} transfections per replicate)`, code: x.id}})
@@ -174,6 +181,7 @@ onMounted(async() => {
             return pelletName ? `${wellCoordinate}:<br>${pelletName}` : wellCoordinate
         },
     }
+    plateLayout.selectionTableRef.value = crudTable.tableRef.value
 })
 
 async function refreshExperiment() {
@@ -301,6 +309,7 @@ async function submitPellets(pellets: DraftPellet[]) {
 
     if (response?.success) {
         toast.add({ severity: 'success', summary: 'Successful', detail: `${response?.data?.length} Records added`, life: 3000 })
+        crudTableKey.value++
 
         // Add pellets to empty wells if storage box is selected
         if (plateWithWellSpecs.value) {
@@ -336,6 +345,219 @@ async function submitPellets(pellets: DraftPellet[]) {
     }
 }
 
+const pelletsWithClause = Object.freeze({
+    harvestedBy: {
+        columns: {
+            name: true
+        },
+    },
+    transfectTarget: {
+        columns: {},
+        with: {
+            target: {
+                columns: {
+                    id: true,
+                    name: true
+                },
+                with: {
+                    region: {
+                        columns: {
+                            name: true
+                        },
+                        with: {
+                            gene: {
+                                columns: {
+                                    symbol: true
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            experiment: {
+                columns: {
+                    id: true,
+                    name: true
+                },
+                with: {
+                    cycle: {
+                        columns: {
+                            name: true
+                        }
+                    }
+                },
+            }
+        }
+    },
+    nucleicAcid: {
+        columns: {
+            id: true
+        },
+    },
+    wellable: {
+        with: {
+            wellContents: {
+                with: {
+                    well: {
+                        columns: {
+                            id: true,
+                            x: true,
+                            y: true,
+                        },
+                        with: {
+                            plate: {
+                                columns: {
+                                    id: true,
+                                    name: true,
+                                    plateType: true,
+                                }
+                            }
+                        }
+                    },
+                },
+            },
+        }
+    },
+})
+
+const columnDefs: ColumnDefinitions = {
+    colorTile:{
+        index: 0,
+        header: '',
+        sortable: false,
+        type: 'element',
+        element: (x: any) => {
+            const wellSpec = plateLayout.getWellSpecBySelectionTableRecordId(x.id)
+            return wellSpec ? `<span
+                class="inline-block w-6 h-6 rounded-sm text-center"
+                style="color: ${getWellTextColor(wellSpec.color)}; background-color:${wellSpec.color}">
+                ${wellSpec.symbol}
+            </span>` : ''
+        },
+        searchable: false,
+        exportable: false,
+    },
+    name: {
+        index: 1,
+    },
+    isBackup: {
+        index: 2,
+    },
+    isCurrent: {
+        index: 3,
+    },
+    nucleicAcid: {
+        header: 'Nucleic Acid',
+        index: 4,
+        type: 'element',
+        element: (x: any) => {
+            const href = _.has(x, 'nucleicAcid.id') ? `/sge/nucleic-acids?pelletId=${x.id}` : null
+            return href ? `<a href="${href}" class="text-blue-500 hover:underline">✓</a>` : ''
+        },
+        exportValue: (x: any) => {
+            return _.has(x, 'nucleicAcid.id') ? 'true' : 'false'
+        },
+    },
+    transfectionExperiment: {
+        path: 'transfectTarget.experiment.cycle.name',
+        index: 5,
+    },
+    wellContents: {
+        header: 'Location',
+        format: (x: any) => {
+            // return _.has(x, 'wellContents.well.plate') ? ` ${_.get(x, 'wellContents.well.plate.name')}: ${wellCoordinateToChar(x.wellContents?.well?.y)}${x.wellContents?.well?.x}` : ''
+            if (!_.isEmpty(x?.wellable?.wellContents)) {
+                return _.map(x.wellable.wellContents, (wellContent) => {
+                    return `${_.get(wellContent, 'well.plate.name')}: ${wellCoordinateToChar(wellContent?.well?.y)}${wellContent?.well?.x}`
+                }).join(', ')
+            } else {
+                return ''
+            }
+        },
+        path: 'wellContents.displayValue',
+        type: 'string',
+        index: 6,
+    },
+    transfectTarget: {
+        header: 'Target',
+        format: (x: any) => { return _.get(x, 'transfectTarget.target.name') || `${_.get(x, 'transfectTarget.target.region.gene.symbol')} : ${_.get(x, 'transfectTarget.target.region.name')}`},
+        path: 'transfectTarget.displayValue',
+        type: 'string',
+        index: 7,
+    },
+    transfectTargetId: {
+        display: false,
+    },
+    harvestedBy: {
+        path: 'harvestedBy.name',
+    },
+}
+const pelletsWhereClause = {
+    '==': [{ 'var': 'transfectTarget.experiment.id' }, route.params.id ]
+}
+const rowActions = {
+    assign: {
+        label: '',
+        icon: 'pi pi-arrow-right',
+        tooltip: 'Assign to selected well',
+        disabled: () => !selectedPlate.value || plateLayout.selectedWells.value.length === 0,
+        action: async (data: any) => {
+            if (plateLayout.selectedWells.value.length > 1) {
+                toast.add({ severity: 'warn', summary: 'Multiple wells selected', detail: 'Please select only one well to assign a pellet.', life: 3000 })
+                return
+            } else if (!_.isEmpty(plateLayout.selectedWells.value[0].data.wellContents)) {
+                toast.add({ severity: 'warn', summary: 'Well already has contents', detail: 'Please select an empty well to assign a pellet.', life: 3000 })
+                return
+            } else {
+                await plateLayout.assignIdToSelectedWells(data.id)
+                crudTable.didUpdateRecord(data)
+            }
+        }
+    }
+}
+const fieldDefs: FieldDefinitions = {
+    transfectTargetId: {
+        label: 'Target',
+        component: 'NestedSelect',
+        props: {
+            parentSearchBaseUrl: `${config.public.apiBase}/transfect-experiments`,
+            parentSearchFields: ['cycle.name'],
+            parentValueField: 'id',
+            parentDisplayFields: ['cycle.name'],
+            parentIftaLabel: 'Experiment',
+            parentSearchWithClause: {
+                cycle: {columns: {name: true}},
+            },
+
+            searchBaseUrl: `${config.public.apiBase}/transfect-targets`,
+            searchFields: ['target.name', 'target.region.gene.symbol', 'target.region.name'],
+            valueField: 'id',
+            displayFormat: (x:any) => { return x.target?.name ?? `${x.target?.region?.gene?.symbol}:${x.target.region.name}`},
+            parentKeyField: 'experimentId',
+            searchWithClause: {
+                target: {columns: {name: true}, with: {region: {columns: {name: true}, with: {gene: {columns: {symbol: true}}}}}},
+            },
+        },
+        readOnly: true,
+    },
+    extractionExperimentId: {
+        label: 'Extraction experiment',
+        component: 'AutoCompleter',
+        props: {
+            searchBaseUrl: `${config.public.apiBase}/extraction-experiments`,
+            searchFields: ['name'],
+            valueField: 'id',
+            displayFields: ['name'],
+            dropdown: true,
+        }
+    },
+    harvestedOn: {
+        readOnly: true,
+    },
+    harvestDay: {
+        readOnly: true,
+    },
+}
 </script>
 <template>
     <div v-if="loaded">
@@ -444,41 +666,21 @@ async function submitPellets(pellets: DraftPellet[]) {
             <div class="col-span-12 space-y-5 mb-5">
                 <Splitter class="h-full overflow-y-hidden" :layout="smallerThanLg ? 'vertical' : 'horizontal'">
                     <SplitterPanel class="overflow-scroll" :size="60">
-                        <DataTable
-                            :value="experiment?.pellets"
-                            v-model:selection="selectedExistingPellets"
-                            tableStyle="min-width: 50rem"
-                            selectionMode="multiple">
-                            <template #header>
-                                <span class="text-xl font-bold">Existing pellets</span>
-                                <Button
-                                    class="ml-2"
-                                    icon="pi pi-trash"
-                                    severity="danger"
-                                    label="Delete"
-                                    outlined
-                                    @click="displayDeleteConfirmation = true"
-                                    :disabled="!selectedExistingPellets || selectedExistingPellets.length == 0" />
-                            </template>
-                            <template #empty> No data </template>
-                            <Column columnKey="selectBox" :reorderableColumn="false" class="w-0 !pl-6" selectionMode="multiple" :exportable="false" frozen />
-                            <Column field="name" header="Name" sortable></Column>
-                            <Column field="isBackup" header="Is Backup" sortable>
-                                <template #body="slotProps">
-                                    {{ slotProps.data.isBackup ? '✓' : '' }}
-                                </template>
-                            </Column>
-                            <Column field="transfections" header="Transfections" sortable>
-                                <template #body="slotProps">
-                                    {{ _.join(slotProps.data.transfections, ', ') }}
-                                </template>
-                            </Column>
-                            <Column field="harvestDay" header="Day" sortable></Column>
-                            <Column field="pctPassaged" header="% passaged" sortable></Column>
-                            <Column field="pctHarvested" header="% harvested" sortable></Column>
-                            <Column field="d3Confluency" header="% D3 confluency" sortable></Column>
-                            <Column field="harvestNotes" header="Notes" sortable></Column>
-                        </DataTable>
+                        <QuickTable
+                            :key="crudTableKey"
+                            :ref="crudTable.setTableRef"
+                            tableName="pellets"
+                            title="Pellets"
+                            schemaName="select"
+                            :columnDefs="columnDefs"
+                            :rowActions="rowActions"
+                            :withClause="pelletsWithClause"
+                            :where="pelletsWhereClause"
+                            :canAdd="false"
+                            :canEditMultiple="true"
+                            @clickedRecordEdit="crudTable.didClickRecordEdit"
+                            @clickedMultipleRecordEdit="crudTable.didClickMultipleRecordEdit"
+                        />
                     </SplitterPanel>
                     <SplitterPanel class="flex flex-col overflow-scroll mt-10" :size="40" :minSize="25" v-if="plateWithWellSpecs">
                         <div class="flex justify-end mr-10 mb-5">
@@ -498,6 +700,14 @@ async function submitPellets(pellets: DraftPellet[]) {
                                 @all-wells-selected="plateLayout.selectedAllWells"
                                 @well-contents-updated="plateLayout.updatedWellContents"
                             >
+                            <template #button1>
+                                <Button
+                                    class="p-button-secondary"
+                                    icon="pi pi-trash"
+                                    v-tooltip="{value: 'Empty selected wells', showDelay: 500}"
+                                    :disabled="_.isEmpty(plateLayout.selectedWells.value)"
+                                    @click="plateLayout.emptySelectedWells" />
+                            </template>
                             </PlateDiagram>
                         </div>
                     </SplitterPanel>
@@ -511,6 +721,27 @@ async function submitPellets(pellets: DraftPellet[]) {
                         <Button label="No" icon="pi pi-times" @click="displayDeleteConfirmation=!displayDeleteConfirmation" text severity="secondary" />
                         <Button label="Yes" icon="pi pi-check" @click="didClickDeleteSelectedRecords" severity="danger" outlined autofocus />
                     </template>
+                </Dialog>
+                <Dialog v-model:visible="showPelletEditDialog" modal header="Edit" class="w-auto" :closable="false">
+                    <QuickForm
+                        v-if="crudTable.state.editingRecordId && crudTable.state.showEditForm"
+                        :recordId="crudTable.state.editingRecordId"
+                        tableName="pellets"
+                        schemaName="update"
+                        :canDelete="false"
+                        :fieldDefs="fieldDefs"
+                        @cancel="crudTable.didClickCancelEditForm"
+                        @recordUpdate="crudTable.didUpdateRecord"
+                    />
+                    <QuickFormMultiple
+                        v-if="crudTable.state.showMultipleEditForm"
+                        tableName="pellets"
+                        :recordIds="crudTable.state.editingMultipleRecordsIds"
+                        schemaName="update"
+                        :fieldDefs="fieldDefs"
+                        @cancel="crudTable.didClickCancelMultipleEditForm"
+                        @records-update="crudTable.didUpdateMultipleRecords"
+                    />
                 </Dialog>
             </div>
         </div>
