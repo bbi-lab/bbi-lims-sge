07e5dc5aefc99c6780178f00430b0f218aab9e5a
David Reinhart <davidrr@uw.edu>
2026-01-22 09:12:13 -0800

    Fixing issue with pellet AutoComplete config on extration experiment page, adjusting size to account for longer pellet names

--- pages/sge/extraction-experiment/[id]/extraction.vue
+++ pages/sge/extraction-experiment/[id]/extraction.vue
@@ -177,12 +177,11 @@ const nucleicAcidFieldDefs: FieldDefinitions = {
         component: 'AutoCompleter',
         props: {
             searchBaseUrl: `${config.public.apiBase}/pellets`,
-            searchFields: ['transfectTarget.experiment.name', 'transfectTarget.target.name'],
-            searchWithClause: {
-                transfectTarget: {columns: {}, with: {experiment: {columns: {name: true}}, target: {columns: {name: true}}}},
-            },
+            searchFields: ['name'],
             valueField: 'id',
-            displayFields: ['transfectTarget.experiment.name', 'transfectTarget.target.name'],
+            displayFields: ['name', 'isBackup'],
+            displayFormat: (x: any) => x.isBackup ? `${x.name} (backup)` : x.name,
+            inputClass: 'w-80',
         }
     },
     dnaConcentration: {
