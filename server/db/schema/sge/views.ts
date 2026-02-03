43d6709563d4d44d1cc90a08df2ddbe7f00bf82e
David Reinhart <davidrr@uw.edu>
2026-02-03 14:36:53 -0800

    Update sequencing run samples SQL view to include RNA samples

--- server/db/schema/sge/views.ts
+++ server/db/schema/sge/views.ts
@@ -295,6 +295,7 @@ export const viewSequencingRunAllSamples = pgView('view_sequencing_run_all_sampl
   sequencingRunId: uuid('sequencing_run_id'),
   sampleType: varchar('sample_type', { enum: ['internal', 'external'] }),
   dnaId: uuid('dna_id'),
+  rnaId: uuid('rna_id'),
   indexPrimer1Id: uuid('index_primer_1_id'),
   indexPrimer2Id: uuid('index_primer_2_id'),
   indexPrimer1Label: varchar('index_primer_1_label'),
@@ -311,10 +312,14 @@ export const viewSequencingRunAllSamples = pgView('view_sequencing_run_all_sampl
   createdAt: timestamp('created_at'),
 }).as(sql`SELECT
   sequencing_run_samples.id AS id,
-  pellets.name AS sample_name,
+  CASE
+    WHEN dna_pellets.name IS NOT NULL THEN dna_pellets.name
+    ELSE rna_pellets.name
+  END AS sample_name,
   sequencing_run_id,
   'internal' AS sample_type,
   dna_id,
+  rna_id,
   NULL AS external_sample_id,
   index_primer_1_id,
   index_primer_2_id,
@@ -337,12 +342,14 @@ export const viewSequencingRunAllSamples = pgView('view_sequencing_run_all_sampl
   sequencing_run_samples.notes AS notes,
   created_at
   FROM sequencing_run_samples
-  JOIN dna ON sequencing_run_samples.dna_id = dna.id
-  JOIN pellets ON dna.pellet_id = pellets.id
-  JOIN index_primers AS primer1 ON sequencing_run_samples.index_primer_1_id = primer1.id
-  JOIN index_primers AS primer2 ON sequencing_run_samples.index_primer_2_id = primer2.id
-  JOIN wells ON sequencing_run_samples.source_well_id = wells.id
-  JOIN plates ON wells.plate_id = plates.id
+  LEFT JOIN dna ON sequencing_run_samples.dna_id = dna.id
+  LEFT JOIN rna ON sequencing_run_samples.rna_id = rna.id
+  LEFT JOIN pellets AS dna_pellets ON dna.pellet_id = dna_pellets.id
+  LEFT JOIN pellets AS rna_pellets ON rna.pellet_id = rna_pellets.id
+  LEFT JOIN index_primers AS primer1 ON sequencing_run_samples.index_primer_1_id = primer1.id
+  LEFT JOIN index_primers AS primer2 ON sequencing_run_samples.index_primer_2_id = primer2.id
+  LEFT JOIN wells ON sequencing_run_samples.source_well_id = wells.id
+  LEFT JOIN plates ON wells.plate_id = plates.id
   UNION
   SELECT
   sequencing_run_external_samples.id AS id,
@@ -350,6 +357,7 @@ export const viewSequencingRunAllSamples = pgView('view_sequencing_run_all_sampl
   sequencing_run_id,
   'external' AS sample_type,
   NULL AS dna_id,
+  NULL AS rna_id,
   external_sample_id,
   index_primer_1_id,
   index_primer_2_id,
