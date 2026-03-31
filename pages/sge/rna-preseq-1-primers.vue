0af4faf6d6b290741f5bad5723585f679f40ddd9
David Reinhart <davidrr@uw.edu>
2026-03-31 08:52:15 -0700

    Update RNA Preseq-1 primers page to allow imports

--- pages/sge/rna-preseq-1-primers.vue
+++ pages/sge/rna-preseq-1-primers.vue
@@ -11,6 +11,42 @@ const route = useRoute()
 const tableKey = ref<string>(uuidv4())
 const whereClauses = ref()
 const readonlyValues = ref<Record<string, any>>({})
+const importDialogVisible = ref(false)
+const toast = useToast()
+
+const submitRnaPreseq1Primers = async (data: any[]) => {
+    try {
+        // remove items with "Sample row" in the notes field as these are template row
+        const filteredData = _.filter(data, (item) => {
+            return !_.includes(_.toLower(item.notes || ''), 'sample row')
+        })
+        if (_.isEmpty(filteredData)) {
+            toast.add({ severity: 'warn', summary: 'No records found', life: 5000 })
+            return
+        }
+        const response: { primers: { id: string }[]; insertedCount: number } = await $fetch(`${config.public.apiBase}/custom/primers/rna-preseq1-primers/pcr-primer-import`, {
+            method: 'POST',
+            body: filteredData,
+        })
+        crudTable.didAddRecords(response.primers)
+        toast.add({ severity: 'success', summary: 'Success', detail: `Successfully imported ${response.insertedCount} RNA PreSeq 1 Primers.`, life: 5000 })
+        importDialogVisible.value = false
+    } catch (error: any) {
+        const userMessage =  _.isArray(error?.data?.data) ? convertErrorDataToUserMessage(error.data.data) : error.statusMessage ?? 'An unexpected error occurred during import. Please try again.'
+        toast.add({ severity: 'error', summary: 'Error', detail: userMessage, life: 5000 })
+    }
+}
+
+const importRnaPreseq1Primers = (event: any) => {
+    try {
+        const files = event.files
+        const f = files[0]
+
+        fileToSheet(f, submitRnaPreseq1Primers)
+    } catch (error) {
+        console.error('Error importing RNA PreSeq 1 Primers:', error)
+    }
+}
 
 watch(() => route.query, async (newValue, oldValue) => {
     const queryParamFilters = _.map(newValue, (val, key) => {
@@ -164,7 +200,16 @@ const formWithClause = {
                 @clickedRecordEdit="crudTable.didClickRecordEdit"
                 @clickedRecordAdd="crudTable.didClickRecordAdd"
                 @clickedMultipleRecordEdit="crudTable.didClickMultipleRecordEdit"
-            />
+            >
+                <template #header-buttons>
+                    <Button
+                        v-if="!crudTable.state.showAddForm && !crudTable.state.showEditForm && !crudTable.state.showMultipleEditForm"
+                        label="Import from CSV"
+                        icon="pi pi-file-import"
+                        @click="importDialogVisible = true"
+                    />
+                </template>
+            </QuickTable>
         </SplitterPanel>
          <SplitterPanel v-if="crudTable.state.showAddForm || crudTable.state.showEditForm || crudTable.state.showMultipleEditForm">
             <QuickForm
@@ -200,4 +245,34 @@ const formWithClause = {
             />
         </SplitterPanel>
     </Splitter>
+
+    <Dialog v-model:visible="importDialogVisible" modal :closable="false" :style="{ width: '35' }">
+        <slot name="closebutton">
+            <div class="flex justify-end">
+                 <Button icon="pi pi-times" class="p-button-rounded p-button-text p-button-plain ml-auto mr-0" @click="importDialogVisible = false" />
+            </div>
+        </slot>
+        <slot name="header">
+            <span class="flex justify-center mt-3 font-bold">Import RNA PreSeq 1 Primers from CSV</span>
+        </slot>
+        <a href="/templates/pcr1_primer_import_template.xlsx" download class="flex justify-center mt-3 mb-5 text-primary">Download template</a>
+        <FileUpload
+            mode="basic"
+            accept="application/msexcel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, text/csv"
+            class="p-button-info"
+            :maxFileSize="1000000"
+            :customUpload="true"
+            :auto="true"
+            @uploader="importRnaPreseq1Primers"
+            chooseLabel="Upload"
+            v-tooltip="{value: 'Upload RNA PreSeq 1 Primers', showDelay: 500}"
+        >
+            <template #chooseicon>
+                <i class="pi pi-upload"></i>
+            </template>
+            <template #uploadicon>
+                <i class="pi pi-upload"></i>
+            </template>
+        </FileUpload>
+    </Dialog>
 </template>
