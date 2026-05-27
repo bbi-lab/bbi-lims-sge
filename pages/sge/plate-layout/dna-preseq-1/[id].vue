68ffaee9b042e2695f436d7ce562672e9292b77f
David Reinhart <davidrr@uw.edu>
2026-05-27 15:35:49 -0700

    fix(dna-preseq-1): update plate layout auto-layout feature to only assign negative control to 2 wells

--- pages/sge/plate-layout/dna-preseq-1/[id].vue
+++ pages/sge/plate-layout/dna-preseq-1/[id].vue
@@ -223,7 +223,7 @@ const layoutPreseq1 = async () => {
 
     if (negativeControl) {
         // assign to the first column (8 wells)
-        const wellsToAssignTo = _.filter(plateWithWellSpecs.value.wells, (well) => well.x == 1)
+        const wellsToAssignTo = _.filter(plateWithWellSpecs.value.wells, (well) => well.x == 1 && well.y <= 2)
         const wellContentsToAdd = _.map(wellsToAssignTo, (well) => {
             return {
                 wellId: well.id,
