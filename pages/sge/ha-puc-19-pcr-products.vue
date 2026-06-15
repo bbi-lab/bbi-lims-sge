ee65e6b01279b9a931b0d2d658a61806ad52e5bb
David Reinhart <davidrr@uw.edu>
2026-06-15 08:23:19 -0700

    fix: change misnamed field on HA Puc19 PCR Products page

--- pages/sge/ha-puc-19-pcr-products.vue
+++ pages/sge/ha-puc-19-pcr-products.vue
@@ -45,11 +45,12 @@ const columnDefs = {
     haPcrProductId: { display: false },
     haPuc19PrimerForwardId: { display: false },
     haPuc19PrimerReverseId: { display: false },
+    haPuc19GibsonProducts: { display: false },
     temperatureUsed: {
         header: 'Temp. Used (°C)',
     },
     cleanedBy: {
-        path: 'performedBy.name'
+        path: 'cleanedBy.name'
     },
 }
 const fieldDefs: FieldDefinitions = {
@@ -95,7 +96,7 @@ const fieldDefs: FieldDefinitions = {
     wtHap1DnaConcentration: {
         label: 'WT HAP1 DNA Concentration (ng/µL)',
     },
-    performedBy: {
+    cleanedBy: {
         component: 'AutoCompleter',
         props: {
             searchBaseUrl: `${config.public.apiBase}/users`,
